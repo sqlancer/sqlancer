9a54d8f19e503e6dad45e87164f069def7f1b4ee
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2019-05-02 16:12:15 +0200

    Allow inserting multiple values at once

--- src/lama/tablegen/sqlite3/Sqlite3RowGenerator.java
+++ src/lama/tablegen/sqlite3/Sqlite3RowGenerator.java
@@ -35,26 +35,47 @@ public class Sqlite3RowGenerator {
 			sb.append(" DEFAULT VALUES");
 		} else {
 			sb.append("(");
-			List<Column> columns = table.getColumns();
-			for (int i = 0; i < columns.size(); i++) {
-				if (i != 0) {
-					sb.append(", ");
-				}
-				sb.append(columns.get(i).getName());
-			}
-			sb.append(") VALUES (");
-			for (int i = 0; i < columns.size(); i++) {
-				if (i != 0) {
-					sb.append(", ");
-				}
-				Expression randomLiteral = SQLite3ExpressionGenerator.getRandomLiteralValue(false);
-				SQLite3Visitor visitor = new SQLite3Visitor();
-				visitor.visit(randomLiteral);
-				sb.append(visitor.get());
-			}
+			List<Column> columns = appendColumnNames(table, sb);
 			sb.append(")");
+			sb.append(" VALUES ");
+			int nrValues = 1 + Randomly.smallNumber();
+			appendNrValues(sb, columns, nrValues);
 		}
 		return sb.toString();
+	}
+
+	private static void appendNrValues(StringBuilder sb, List<Column> columns, int nrValues) {
+		for (int i = 0; i < nrValues; i++) {
+			if (i != 0) {
+				sb.append(", ");
+			}
+			sb.append("(");
+			appendValue(sb, columns);
+			sb.append(")");
+		}
+	}
+
+	private static void appendValue(StringBuilder sb, List<Column> columns) {
+		for (int i = 0; i < columns.size(); i++) {
+			if (i != 0) {
+				sb.append(", ");
+			}
+			Expression randomLiteral = SQLite3ExpressionGenerator.getRandomLiteralValue(false);
+			SQLite3Visitor visitor = new SQLite3Visitor();
+			visitor.visit(randomLiteral);
+			sb.append(visitor.get());
+		}
+	}
+
+	private static List<Column> appendColumnNames(Table table, StringBuilder sb) {
+		List<Column> columns = table.getColumns();
+		for (int i = 0; i < columns.size(); i++) {
+			if (i != 0) {
+				sb.append(", ");
+			}
+			sb.append(columns.get(i).getName());
+		}
+		return columns;
 	}
 
 }
