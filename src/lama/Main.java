efff8418ea61878c3b16915aa873066656b19124
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2019-05-03 20:41:12 +0200

    Extend the column constraints

--- src/lama/Main.java
+++ src/lama/Main.java
@@ -43,7 +43,7 @@ public class Main {
 	private static final int TOTAL_NR_THREADS = 100;
 	private static final int NR_CONCURRENT_THREADS = 16;
 	public static final int NR_INSERT_ROW_TRIES = 50;
-	public static final int EXPRESSION_MAX_DEPTH = 3;
+	public static final int EXPRESSION_MAX_DEPTH = 2;
 	public static final File LOG_DIRECTORY = new File("logs");
 	static volatile AtomicLong nrQueries = new AtomicLong();
 
--- src/lama/tablegen/sqlite3/SQLite3TableGenerator.java
+++ src/lama/tablegen/sqlite3/SQLite3TableGenerator.java
@@ -6,12 +6,25 @@ import lama.Main.StateToReproduce;
 import lama.Randomly;
 import lama.sqlite3.SQLite3Visitor;
 
+/**
+ * See https://www.sqlite.org/lang_createtable.html#rowid
+ *
+ * TODO What's missing:
+ * <ul>
+ * <li>TEMP tables (should be supported?)</li>
+ * <li>create table as select</li>
+ * <li>table constraints</li>
+ * <li>foreign key constraints</li>
+ * </ul>
+ */
 public class SQLite3TableGenerator {
 
 	private final StringBuilder sb = new StringBuilder();
 	private final String tableName;
 	private int columnId;
 	private boolean containsPrimaryKey;
+	private boolean containsAutoIncrement;
+	private boolean conflictClauseInserted;
 
 	public SQLite3TableGenerator(String tableName) {
 		this.tableName = tableName;
@@ -48,7 +61,7 @@ public class SQLite3TableGenerator {
 			columnId++;
 		}
 		sb.append(")");
-		if (containsPrimaryKey) {
+		if (containsPrimaryKey && !containsAutoIncrement) {
 			if (Randomly.getBoolean()) {
 				// see https://sqlite.org/withoutrowid.html
 				sb.append(" WITHOUT ROWID");
@@ -56,9 +69,8 @@ public class SQLite3TableGenerator {
 		}
 	}
 
-
 	private enum Constraints {
-		NONE, NOT_NULL, PRIMARY_KEY, UNIQUE
+		NOT_NULL, PRIMARY_KEY, UNIQUE, CHECK
 	}
 
 	private void createColumn(boolean allowPrimaryKeyInColumn) {
@@ -67,41 +79,71 @@ public class SQLite3TableGenerator {
 		sb.append(" ");
 		String dataType = Randomly.fromOptions("INT", "TEXT", "BLOB", "REAL", "INTEGER");
 		sb.append(dataType);
-		
+
 		if (Randomly.getBoolean()) {
 			List<Constraints> constraints = Randomly.subset(Constraints.values());
 			for (Constraints c : constraints) {
 				switch (c) {
-				case NONE:
-					break;
 				case PRIMARY_KEY:
 					// only one primary key is allow if not specified as table constraint
 					if (allowPrimaryKeyInColumn && !containsPrimaryKey) {
 						sb.append(" PRIMARY KEY");
 						containsPrimaryKey = true;
+						boolean hasOrdering = Randomly.getBoolean();
+						if (hasOrdering) {
+							if (Randomly.getBoolean()) {
+								sb.append(" ASC");
+							} else {
+								sb.append(" DESC");
+							}
+						}
+						if (Randomly.getBoolean()) {
+							insertOnConflictClause();
+						}
+						if (!hasOrdering && dataType.equals("INTEGER")) {
+							if (Randomly.getBoolean()) {
+								containsAutoIncrement = true;
+								sb.append(" AUTOINCREMENT");
+							}
+						}
 					}
 					break;
 				case UNIQUE:
 					sb.append(" UNIQUE");
+					if (Randomly.getBoolean()) {
+						insertOnConflictClause();
+					}
 					break;
 				case NOT_NULL:
 					sb.append(" NOT NULL");
+					if (Randomly.getBoolean()) {
+						insertOnConflictClause();
+					}
+					break;
+				case CHECK:
+					sb.append(" CHECK ( "
+							+ SQLite3Visitor.asString(SQLite3ExpressionGenerator.getRandomLiteralValue(false)) + ")");
 					break;
 				default:
 					throw new AssertionError();
 				}
 			}
-			
 		}
 		if (Randomly.getBoolean()) {
 			sb.append(" DEFAULT " + SQLite3Visitor.asString(SQLite3ExpressionGenerator.getRandomLiteralValue(false)));
 		}
 		if (Randomly.getBoolean()) {
-			sb.append(" CHECK ( " + SQLite3Visitor.asString(SQLite3ExpressionGenerator.getRandomLiteralValue(false)) + ")");
-		}
-		if (Randomly.getBoolean()) {
 			String randomCollate = SQLite3Common.getRandomCollate();
 			sb.append(randomCollate);
+		}
+	}
+
+	// it seems that only one conflict clause can be inserted
+	private void insertOnConflictClause() {
+		if (!conflictClauseInserted) {
+			sb.append(" ON CONFLICT ");
+			sb.append(Randomly.fromOptions("ROLLBACK", "ABORT", "FAIL", "IGNORE", "REPLACE"));
+			conflictClauseInserted = true;
 		}
 	}
 
