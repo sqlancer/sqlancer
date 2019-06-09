fabc8c7aa5888549b0e91c5d7a81dcc1cfae9602
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2019-06-09 18:27:27 +0200

    Implement additional functions

--- src/lama/sqlite3/ast/SQLite3Function.java
+++ src/lama/sqlite3/ast/SQLite3Function.java
@@ -2,12 +2,12 @@ package lama.sqlite3.ast;
 
 import lama.Randomly;
 import lama.sqlite3.gen.SQLite3Cast;
+import lama.sqlite3.gen.SQLite3ExpressionGenerator;
 import lama.sqlite3.schema.SQLite3DataType;
 import lama.sqlite3.schema.SQLite3Schema.Column.CollateSequence;
 
 public class SQLite3Function extends SQLite3Expression {
-	
-	
+
 	private final ComputableFunction func;
 	private final SQLite3Expression[] args;
 
@@ -22,7 +22,6 @@ public class SQLite3Function extends SQLite3Expression {
 		ABS(1) {
 			@Override
 			public SQLite3Constant apply(SQLite3Constant... args) {
-				assert args.length == 1;
 				SQLite3Constant castValue = SQLite3Cast.castToInt(args[0]);
 				if (castValue.isNull()) {
 					return castValue;
@@ -34,6 +33,76 @@ public class SQLite3Function extends SQLite3Expression {
 					double absVal = Math.abs(castValue.asDouble());
 					return SQLite3Constant.createRealConstant(absVal);
 				}
+			}
+		},
+
+		LOWER(1) {
+			@Override
+			public SQLite3Constant apply(SQLite3Constant... args) {
+				if (args[0].getDataType() == SQLite3DataType.TEXT) {
+					String text = args[0].asString().toLowerCase();
+					return SQLite3Constant.createTextConstant(text);
+				} else {
+					return args[0];
+				}
+			}
+		},
+		LIKELY(1) {
+			@Override
+			public SQLite3Constant apply(SQLite3Constant... args) {
+				return args[0];
+			}
+		},
+		IFNULL(2) {
+			@Override
+			public SQLite3Constant apply(SQLite3Constant... args) {
+				for (SQLite3Expression arg : args) {
+					if (!arg.getExpectedValue().isNull()) {
+						return arg.getExpectedValue();
+					}
+				}
+				return SQLite3Constant.createNullConstant();
+			}
+		},
+
+		UPPER(1) {
+
+			@Override
+			public SQLite3Constant apply(SQLite3Constant... args) {
+				if (args[0].getDataType() == SQLite3DataType.TEXT) {
+					String text = args[0].asString().toUpperCase();
+					return SQLite3Constant.createTextConstant(text);
+				} else {
+					return args[0];
+				}
+			}
+
+		},
+		TYPEOF(1) {
+
+			@Override
+			public SQLite3Constant apply(SQLite3Constant... args) {
+				switch (args[0].getDataType()) {
+				case BINARY:
+					return SQLite3Constant.createTextConstant("blob");
+				case INT:
+					return SQLite3Constant.createTextConstant("integer");
+				case NULL:
+					return SQLite3Constant.createTextConstant("null");
+				case REAL:
+					return SQLite3Constant.createTextConstant("real");
+				case TEXT:
+					return SQLite3Constant.createTextConstant("text");
+				default:
+					throw new AssertionError(args[0]);
+				}
+			}
+
+		},
+		UNLIKELY(1) {
+			@Override
+			public SQLite3Constant apply(SQLite3Constant... args) {
+				return args[0];
 			}
 		};
 
