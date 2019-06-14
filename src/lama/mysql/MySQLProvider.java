778570e35c6abf48cf5998fb5df90a318471cbbf
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2019-06-14 23:56:47 +0200

    Implement the action selection logic used for SQLite3

--- src/lama/mysql/MySQLProvider.java
+++ src/lama/mysql/MySQLProvider.java
@@ -2,8 +2,11 @@ package lama.mysql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import lama.DatabaseProvider;
 import lama.Main.QueryManager;
@@ -12,46 +15,144 @@ import lama.Main.StateToReproduce;
 import lama.Query;
 import lama.QueryAdapter;
 import lama.Randomly;
+import lama.mysql.MySQLSchema.MySQLTable;
 import lama.mysql.gen.MySQLRowInserter;
 import lama.mysql.gen.MySQLSetGenerator;
 import lama.mysql.gen.MySQLTableGenerator;
+import lama.sqlite3.gen.QueryGenerator;
 import lama.sqlite3.gen.SQLite3Common;
+import lama.sqlite3.gen.SQLite3ReindexGenerator;
 
 public class MySQLProvider implements DatabaseProvider {
 
+	private static final int NR_QUERIES_PER_TABLE = 10000;
+	private static final int MAX_INSERT_ROW_TRIES = 3;
 	private final Randomly r = new Randomly();
+	private QueryManager manager;
 
-	enum Actions {
-		SHOW_TABLES;
+	enum Action {
+		SHOW_TABLES, INSERT, SET_VARIABLE;
 	}
 
 	@Override
 	public void generateAndTestDatabase(String databaseName, Connection con, StateLogger logger, StateToReproduce state,
 			QueryManager manager) throws SQLException {
 
+		this.manager = manager;
 		for (int i = 0; i < Randomly.smallNumber() + 1; i++) {
 			String tableName = SQLite3Common.createTableName(i);
 			Query createTable = MySQLTableGenerator.generate(tableName, r);
 			manager.execute(createTable);
 		}
 		MySQLSchema newSchema = MySQLSchema.fromConnection(con, databaseName);
-		for (int i = 0; i < 1000; i++) {
-			Query q = MySQLRowInserter.insertRow(newSchema.getRandomTable(), r);
-			manager.execute(q);
+
+		int[] nrRemaining = new int[Action.values().length];
+		List<Action> actions = new ArrayList<>();
+		int total = 0;
+		for (int i = 0; i < Action.values().length; i++) {
+			Action action = Action.values()[i];
+			int nrPerformed = 0;
+			switch (action) {
+			case SHOW_TABLES:
+				nrPerformed = r.getInteger(0, 1);
+				break;
+			case INSERT:
+				nrPerformed = 100;
+				break;
+			case SET_VARIABLE:
+				nrPerformed = r.getInteger(0, 10);
+				break;
+			}
+			if (nrPerformed != 0) {
+				actions.add(action);
+			}
+			nrRemaining[action.ordinal()] = nrPerformed;
+			total += nrPerformed;
 		}
-		for (int i = 0; i < 1000; i++) {
-			Query q = MySQLSetGenerator.set(r);
-			manager.execute(q);
-		}
-		for (int i = 0; i < 1000; i++) {
-			try (Statement s = con.createStatement()) {
-				Query q = new QueryAdapter("SHOW TABLES");
-				manager.execute(q);
+		
+		while (total != 0) {
+			Action nextAction = null;
+			int selection = r.getInteger(0, total);
+			int previousRange = 0;
+			for (int i = 0; i < nrRemaining.length; i++) {
+				if (previousRange <= selection && selection < previousRange + nrRemaining[i]) {
+					nextAction = Action.values()[i];
+					break;
+				} else {
+					previousRange += nrRemaining[i];
+				}
+			}
+			assert nextAction != null;
+			assert nrRemaining[nextAction.ordinal()] > 0;
+			nrRemaining[nextAction.ordinal()]--;
+			Query query;
+			switch (nextAction) {
+			case SHOW_TABLES:
+				 query = new QueryAdapter("SHOW TABLES");
+				 break;
+			case INSERT:
+				 query = MySQLRowInserter.insertRow(newSchema.getRandomTable(), r);
+				 break;
+			case SET_VARIABLE:
+				query = MySQLSetGenerator.set(r);
+				break;
+			default:
+				throw new AssertionError(nextAction);
+			}
+			try {
+				manager.execute(query);
+				if (query.couldAffectSchema()) {
+					newSchema = MySQLSchema.fromConnection(con, databaseName);
+				}
+			} catch (Throwable t) {
+				System.err.println(query.getQueryString());
+				throw t;
+			}
+			total--;
+			for (MySQLTable t : newSchema.getDatabaseTables()) {
+				if (!ensureTableHasRows(con, t, r)) {
+					return;
+				}
+			}
+			if (Randomly.getBoolean()) {
+				SQLite3ReindexGenerator.executeReindex(con, state);
+			}
+			newSchema = MySQLSchema.fromConnection(con, databaseName);
+
+			MySQLQueryGenerator queryGenerator = new MySQLQueryGenerator(manager, r);
+			for (int i = 0; i < NR_QUERIES_PER_TABLE; i++) {
+				queryGenerator.generateAndCheckQuery(state, logger);
+				manager.incrementSelectQueryCount();
 			}
 		}
 
 	}
 
+	private boolean ensureTableHasRows(Connection con, MySQLTable randomTable, Randomly r) throws SQLException {
+		int nrRows;
+		int counter = MAX_INSERT_ROW_TRIES;
+		do {
+			try {
+				Query q = MySQLRowInserter.insertRow(randomTable, r);
+				manager.execute(q);
+			} catch (SQLException e) {
+				if (!QueryGenerator.shouldIgnoreException(e)) {
+					throw new AssertionError(e);
+				}
+			}
+			nrRows = getNrRows(con, randomTable);
+		} while (nrRows == 0 && counter-- != 0);
+		return nrRows != 0;
+	}
+
+	public static int getNrRows(Connection con, MySQLTable table) throws SQLException {
+		try (Statement s = con.createStatement()) {
+			try (ResultSet query = s.executeQuery("SELECT COUNT(*) FROM " + table.getName())) {
+				query.next();
+				return query.getInt(1);
+			}
+		}
+	}
 	@Override
 	public Connection createDatabase(String databaseName) throws SQLException {
 		String url = "jdbc:mysql://localhost:3306/?serverTimezone=UTC&useSSL=false";
