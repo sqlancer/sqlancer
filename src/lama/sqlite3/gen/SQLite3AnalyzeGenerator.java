bc72699ff9f9cb19cdd38349d6ee5ff808b9e4fa
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2019-07-29 21:15:20 +0200

    Support ANALYZE for a single table

--- src/lama/sqlite3/gen/SQLite3AnalyzeGenerator.java
+++ src/lama/sqlite3/gen/SQLite3AnalyzeGenerator.java
@@ -2,11 +2,19 @@ package lama.sqlite3.gen;
 
 import lama.Query;
 import lama.QueryAdapter;
+import lama.Randomly;
+import lama.sqlite3.schema.SQLite3Schema;
 
 public class SQLite3AnalyzeGenerator {
-	
-	public static Query generateAnalyze() {
-		return new QueryAdapter("ANALYZE");
+
+	public static Query generateAnalyze(SQLite3Schema newSchema) {
+		StringBuilder sb = new StringBuilder("ANALYZE");
+		if (Randomly.getBoolean()) {
+			sb.append(" ");
+			sb.append(newSchema.getRandomTable().getName());
+			// TODO index
+		}
+		return new QueryAdapter(sb.toString());
 	}
 
 }
