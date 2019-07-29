11bf97760f77993b5e2be47fbd5508821b34d3bc
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2019-07-29 21:14:33 +0200

    Extend REINDEX

--- src/lama/sqlite3/gen/SQLite3ReindexGenerator.java
+++ src/lama/sqlite3/gen/SQLite3ReindexGenerator.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 
 import lama.Query;
 import lama.QueryAdapter;
+import lama.Randomly;
 import lama.StateToReproduce;
+import lama.sqlite3.schema.SQLite3Schema;
 
 /**
  * @see https://www.sqlite.org/lang_reindex.html
@@ -12,7 +14,16 @@ import lama.StateToReproduce;
 public class SQLite3ReindexGenerator {
 
 	// only works for the main schema
-	public static Query executeReindex(Connection con, StateToReproduce state) {
+	public static Query executeReindex(Connection con, StateToReproduce state, SQLite3Schema s) {
+		StringBuilder sb = new StringBuilder("REINDEX");
+		if (Randomly.getBoolean()) {
+			sb.append(" ");
+			sb.append(s.getRandomTable().getName());
+		} else {
+			sb.append(Randomly.fromOptions("BINARY", "NOCASE", "RTRIM"));
+		}
+		// TODO index
 		return new QueryAdapter("REINDEX;");
+
 	}
 }
