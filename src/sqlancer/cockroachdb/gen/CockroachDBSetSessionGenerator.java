eed144a08dac8bc18b6a3031870f4287686aab2f
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2020-04-04 15:43:06 +0200

    [CockroachDB] Add additional session variables

--- src/sqlancer/cockroachdb/gen/CockroachDBSetSessionGenerator.java
+++ src/sqlancer/cockroachdb/gen/CockroachDBSetSessionGenerator.java
@@ -20,16 +20,17 @@ public class CockroachDBSetSessionGenerator {
 	private enum CockroachDBSetting {
 		BYTEA_OUTPUT((g) -> Randomly.fromOptions("hex", "escape", "base64")),
 		DEFAULT_INT_SIZE((g) -> Randomly.fromOptions(4, 8)),
-//		ENABLE_ZIG_ZAG_JOIN(CockroachDBSetSessionGenerator::onOff),
 		DISTSQL((g) -> Randomly.fromOptions("on", "off", "auto", "always")),
-//		EXPERIMENTAL_FORCE_SPLIT_AT(CockroachDBSetSessionGenerator::onOff),
+		ENABLE_IMPLICIT_SELECT_FOR_UPDATE(CockroachDBSetSessionGenerator::onOff),
+		ENABLE_INSERT_FAST_PATH(CockroachDBSetSessionGenerator::onOff),
+		ENABLE_ZIGZAG_JOIN(CockroachDBSetSessionGenerator::onOff),
+//		EXPERIMENTAL_ENABLE_HASH_SHARDED_INDEXES(CockroachDBSetSessionGenerator::onOff),
 		EXPERIMENTAL_SERIAL_NORMALIZATION((g) -> Randomly.fromOptions("'rowid'", "'virtual_sequence'")),
-//		EXPERIMENTAL_REORDER_JOINS_LIMIT((g) -> Randomly.getNonCachedInteger()),
-		VECTORIZE((g) -> Randomly.fromOptions("auto", "on", "off")),  /* see https://github.com/cockroachdb/cockroach/issues/44133, https://github.com/cockroachdb/cockroach/issues/44207 */
-//		EXTRA_FLOAT_DIGITS((g) -> g.getRandomly().getInteger(-15, 3)),
+		EXTRA_FLOAT_DIGITS((g) -> g.getRandomly().getInteger(-15, 3)),
 		REORDER_JOINS_LIMIT((g) -> g.getRandomly().getInteger(0, Integer.MAX_VALUE)),
 		SQL_SAFE_UPDATES(CockroachDBSetSessionGenerator::onOff),
 //		TRACING(CockroachDBSetSessionGenerator::onOff)
+		VECTORIZE((g) -> Randomly.fromOptions("auto", "on", "off")),  /* see https://github.com/cockroachdb/cockroach/issues/44133, https://github.com/cockroachdb/cockroach/issues/44207 */
 		;
 		
 		private Function<CockroachDBGlobalState, Object> f;
