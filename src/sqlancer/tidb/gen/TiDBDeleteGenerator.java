b6dd54d79a57ac77a04adc18f80795118dc392e9
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2020-04-15 10:43:57 +0200

    [TiDB] Enhance DELETE

--- src/sqlancer/tidb/gen/TiDBDeleteGenerator.java
+++ src/sqlancer/tidb/gen/TiDBDeleteGenerator.java
@@ -3,10 +3,12 @@ package sqlancer.tidb.gen;
 import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import sqlancer.Query;
 import sqlancer.QueryAdapter;
 import sqlancer.Randomly;
+import sqlancer.tidb.TiDBErrors;
 import sqlancer.tidb.TiDBExpressionGenerator;
 import sqlancer.tidb.TiDBProvider.TiDBGlobalState;
 import sqlancer.tidb.TiDBSchema.TiDBTable;
@@ -18,13 +20,32 @@ public class TiDBDeleteGenerator {
 		Set<String> errors = new HashSet<>();
 		TiDBTable table = globalState.getSchema().getRandomTable(t -> !t.isView());
 		TiDBExpressionGenerator gen = new TiDBExpressionGenerator(globalState).setColumns(table.getColumns());
-		StringBuilder sb = new StringBuilder("DELETE FROM ");
+		StringBuilder sb = new StringBuilder("DELETE ");
+		if (Randomly.getBooleanWithSmallProbability()) {
+			sb.append("LOW_PRIORITY ");
+		}
+		if (Randomly.getBooleanWithSmallProbability()) {
+			sb.append("QUICK ");
+		}
+		if (Randomly.getBooleanWithSmallProbability()) {
+			sb.append("IGNORE ");
+		}
+		sb.append("FROM ");
 		sb.append(table.getName());
 		if (Randomly.getBoolean()) {
 			sb.append(" WHERE ");
 			sb.append(TiDBVisitor.asString(gen.generateExpression()));
 			errors.add("Truncated incorrect");
 			errors.add("Data truncation");
+		}
+		if (Randomly.getBoolean()) {
+			sb.append(" ORDER BY ");
+			TiDBErrors.addExpressionErrors(errors);
+			sb.append(gen.generateOrderBys().stream().map(o -> TiDBVisitor.asString(o)).collect(Collectors.joining(", ")));
+		}
+		if (Randomly.getBoolean()) {
+			sb.append(" LIMIT ");
+			sb.append(Randomly.getNotCachedInteger(0, Integer.MAX_VALUE));
 		}
 		return new QueryAdapter(sb.toString(), errors);
 		
