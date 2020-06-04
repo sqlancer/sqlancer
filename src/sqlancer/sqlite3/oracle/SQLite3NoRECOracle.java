aed8f36d2eabf8c0ed2e462ea23de2d7e234c38f
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2020-06-04 14:39:47 +0200

    [SQLite] Refactor the NoREC implementation

--- src/sqlancer/sqlite3/oracle/SQLite3NoRECOracle.java
+++ src/sqlancer/sqlite3/oracle/SQLite3NoRECOracle.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -18,6 +19,7 @@ import sqlancer.TestOracle;
 import sqlancer.sqlite3.SQLite3Errors;
 import sqlancer.sqlite3.SQLite3Provider.SQLite3GlobalState;
 import sqlancer.sqlite3.SQLite3Visitor;
+import sqlancer.sqlite3.ast.SQLite3Aggregate;
 import sqlancer.sqlite3.ast.SQLite3Expression;
 import sqlancer.sqlite3.ast.SQLite3Expression.Join;
 import sqlancer.sqlite3.ast.SQLite3Expression.SQLite3ColumnName;
@@ -25,7 +27,6 @@ import sqlancer.sqlite3.ast.SQLite3Expression.SQLite3PostfixText;
 import sqlancer.sqlite3.ast.SQLite3Expression.SQLite3PostfixUnaryOperation;
 import sqlancer.sqlite3.ast.SQLite3Expression.SQLite3PostfixUnaryOperation.PostfixUnaryOperator;
 import sqlancer.sqlite3.ast.SQLite3Select;
-import sqlancer.sqlite3.ast.SQLite3Select.SelectType;
 import sqlancer.sqlite3.gen.SQLite3Common;
 import sqlancer.sqlite3.gen.SQLite3ExpressionGenerator;
 import sqlancer.sqlite3.schema.SQLite3Schema;
@@ -35,20 +36,17 @@ import sqlancer.sqlite3.schema.SQLite3Schema.SQLite3Tables;
 
 public class SQLite3NoRECOracle implements TestOracle {
 
-    private static final int NOT_FOUND = -1;
+    private static final int NO_VALID_RESULT = -1;
+    private final SQLite3Schema s;
+    private final Connection con;
+    private final SQLite3StateToReproduce state;
+    private final Set<String> errors = new HashSet<>();
+    private final StateLogger logger;
+    private final MainOptions options;
+    private final SQLite3GlobalState globalState;
     private SQLite3ExpressionGenerator gen;
-    private SQLite3Schema s;
-    private Connection con;
-    private SQLite3StateToReproduce state;
     private String firstQueryString;
     private String secondQueryString;
-    private final Set<String> errors = new HashSet<>();
-    private StateLogger logger;
-    private MainOptions options;
-    private SQLite3GlobalState globalState;
-    private SQLite3Column randomColumnToCheck;
-    Set<String> firstValues = new HashSet<>();
-    Set<String> secondValues = new HashSet<>();
 
     public SQLite3NoRECOracle(SQLite3GlobalState globalState) {
         this.s = globalState.getSchema();
@@ -60,20 +58,16 @@ public class SQLite3NoRECOracle implements TestOracle {
         SQLite3Errors.addExpectedExpressionErrors(errors);
         SQLite3Errors.addMatchQueryErrors(errors);
         SQLite3Errors.addQueryErrors(errors);
-        // aggregate
         errors.add("misuse of aggregate");
         errors.add("misuse of window function");
         errors.add("second argument to nth_value must be a positive integer");
         errors.add("no such table");
-        // FIXME implement
-        errors.add("no query solution"); // INDEXED BY
+        errors.add("no query solution");
         errors.add("unable to use function MATCH in the requested context");
     }
 
     @Override
     public void check() throws SQLException {
-        firstValues.clear();
-        secondValues.clear();
         SQLite3Tables randomTables = s.getRandomTableNonEmptyTables();
         List<SQLite3Column> columns = randomTables.getColumns();
         gen = new SQLite3ExpressionGenerator(globalState).setColumns(columns);
@@ -81,112 +75,105 @@ public class SQLite3NoRECOracle implements TestOracle {
         List<SQLite3Table> tables = randomTables.getTables();
         List<Join> joinStatements = gen.getRandomJoinClauses(tables);
         List<SQLite3Expression> tableRefs = SQLite3Common.getTableRefs(tables, s);
-        randomColumnToCheck = Randomly.fromList(randomTables.getColumns());
-        int firstCount = getFirstQueryCount(con, tableRefs, randomWhereCondition, joinStatements);
-        if (firstQueryString.contains("EXISTS")) {
+        SQLite3Select select = new SQLite3Select();
+        select.setFromTables(tableRefs);
+        select.setJoinClauses(joinStatements);
+
+        int optimizedCount = getOptimizedQuery(select, randomWhereCondition);
+        int unoptimizedCount = getUnoptimizedQuery(select, randomWhereCondition);
+        if (optimizedCount == NO_VALID_RESULT || unoptimizedCount == NO_VALID_RESULT) {
             throw new IgnoreMeException();
         }
-        int secondCount = getSecondQuery(tableRefs, randomWhereCondition, joinStatements);
-        if (firstCount != secondCount && firstCount != NOT_FOUND && secondCount != NOT_FOUND) {
+        if (optimizedCount != unoptimizedCount) {
             state.queryString = firstQueryString + ";\n" + secondQueryString + ";";
-            throw new AssertionError(firstCount + " " + secondCount);
+            throw new AssertionError(optimizedCount + " " + unoptimizedCount);
         }
-        if (firstCount != NOT_FOUND && secondCount != NOT_FOUND
-                && (!firstValues.containsAll(secondValues) || !secondValues.containsAll(firstValues))) {
-            state.queryString = firstQueryString + ";\n" + secondQueryString + ";";
-            throw new AssertionError(firstCount + " " + secondCount + "\n" + firstValues + "\n" + secondValues);
-        }
+
     }
 
-    private int getSecondQuery(List<SQLite3Expression> fromList, SQLite3Expression randomWhereCondition,
-            List<Join> joinStatements) throws SQLException {
-        SQLite3Select select = new SQLite3Select();
-        setRandomOrderBy(select);
+    private int getUnoptimizedQuery(SQLite3Select select, SQLite3Expression randomWhereCondition) throws SQLException {
         SQLite3PostfixUnaryOperation isTrue = new SQLite3PostfixUnaryOperation(PostfixUnaryOperator.IS_TRUE,
                 randomWhereCondition);
         SQLite3PostfixText asText = new SQLite3PostfixText(isTrue, " as count", null);
-        select.setFetchColumns(Arrays.asList(asText, new SQLite3ColumnName(randomColumnToCheck, null)));
-        select.setFromTables(fromList);
-        select.setSelectType(SelectType.ALL);
-        select.setJoinClauses(joinStatements);
-        int secondCount = 0;
-        secondQueryString = SQLite3Visitor.asString(select);
+        select.setFetchColumns(Arrays.asList(asText));
+        secondQueryString = "SELECT SUM(count) FROM (" + SQLite3Visitor.asString(select) + ")";
         if (options.logEachSelect()) {
             logger.writeCurrent(secondQueryString);
         }
         QueryAdapter q = new QueryAdapter(secondQueryString, errors);
-        try (ResultSet rs = q.executeAndGet(con)) {
-            if (rs == null) {
-                return NOT_FOUND;
-            } else {
-                try {
-                    while (rs.next()) {
-                        int val = rs.getInt(1);
-                        if (val == 1) {
-                            secondCount++;
-                            String string = rs.getString(2);
-                            if (string == null) {
-                                secondValues.add("null");
-                            } else {
-                                secondValues.add(string);
-                            }
-                        }
-                    }
-                } catch (SQLException e) {
-                    throw new IgnoreMeException();
-                }
-                rs.getStatement().close();
-            }
-        } catch (Exception e) {
-            if (e instanceof IgnoreMeException) {
-                throw e;
-            }
-            throw new AssertionError(secondQueryString, e);
-        }
-        return secondCount;
+        return extractCounts(q);
     }
 
-    private int getFirstQueryCount(Connection con, List<SQLite3Expression> fromList,
-            SQLite3Expression randomWhereCondition, List<Join> joinStatements) throws SQLException {
-        SQLite3Select select = new SQLite3Select();
-        setRandomOrderBy(select);
-        SQLite3ColumnName aggr = new SQLite3ColumnName(randomColumnToCheck, null);
-        select.setFetchColumns(Arrays.asList(aggr));
-        select.setFromTables(fromList);
+    private int getOptimizedQuery(SQLite3Select select, SQLite3Expression randomWhereCondition) throws SQLException {
+        boolean useAggregate = Randomly.getBoolean();
+        if (Randomly.getBoolean()) {
+            select.setOrderByExpressions(gen.generateOrderBys());
+        }
+        if (useAggregate) {
+            select.setFetchColumns(Arrays.asList(new SQLite3Aggregate(Collections.emptyList(),
+                    SQLite3Aggregate.SQLite3AggregateFunction.COUNT_ALL)));
+        } else {
+            SQLite3ColumnName aggr = new SQLite3ColumnName(SQLite3Column.createDummy("*"), null);
+            select.setFetchColumns(Arrays.asList(aggr));
+        }
         select.setWhereClause(randomWhereCondition);
-        select.setSelectType(SelectType.ALL);
-        select.setJoinClauses(joinStatements);
-        int firstCount = 0;
+        if (useAggregate && Randomly.getBooleanWithSmallProbability()) {
+            select.setGroupByClause(gen.getRandomExpressions(Randomly.smallNumber() + 1));
+        }
         firstQueryString = SQLite3Visitor.asString(select);
         if (options.logEachSelect()) {
             logger.writeCurrent(firstQueryString);
         }
         QueryAdapter q = new QueryAdapter(firstQueryString, errors);
+        return useAggregate ? extractCounts(q) : countRows(q);
+    }
+
+    private int countRows(QueryAdapter q) {
+        int count = 0;
         try (ResultSet rs = q.executeAndGet(con)) {
             if (rs == null) {
-                firstCount = NOT_FOUND;
+                return NO_VALID_RESULT;
             } else {
                 try {
                     while (rs.next()) {
-                        firstCount += 1;
-                        firstValues.add(String.valueOf(rs.getString(1)));
+                        count++;
                     }
-                } catch (Exception e) {
-                    q.checkException(e);
-                    firstCount = NOT_FOUND;
+                } catch (SQLException e) {
+                    count = NO_VALID_RESULT;
                 }
                 rs.getStatement().close();
             }
         } catch (Exception e) {
-            throw new AssertionError(firstQueryString, e);
+            if (e instanceof IgnoreMeException) {
+                throw (IgnoreMeException) e;
+            }
+            throw new AssertionError(secondQueryString, e);
         }
-        return firstCount;
+        return count;
     }
 
-    private void setRandomOrderBy(SQLite3Select select) {
-        if (Randomly.getBoolean()) {
-            select.setOrderByExpressions(gen.generateOrderBys());
+    private int extractCounts(QueryAdapter q) {
+        int count = 0;
+        try (ResultSet rs = q.executeAndGet(con)) {
+            if (rs == null) {
+                return NO_VALID_RESULT;
+            } else {
+                try {
+                    while (rs.next()) {
+                        count += rs.getInt(1);
+                    }
+                } catch (SQLException e) {
+                    count = NO_VALID_RESULT;
+                }
+                rs.getStatement().close();
+            }
+        } catch (Exception e) {
+            if (e instanceof IgnoreMeException) {
+                throw (IgnoreMeException) e;
+            }
+            throw new AssertionError(secondQueryString, e);
         }
+        return count;
     }
 
 }
