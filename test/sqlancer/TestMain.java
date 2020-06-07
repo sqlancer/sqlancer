aacc3d909f07853da532b86cf810142c6a8ce0d2
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2020-06-07 23:56:20 +0200

    Test the NoREC and TLP oracles of DuckDB

--- test/sqlancer/TestMain.java
+++ test/sqlancer/TestMain.java
@@ -6,15 +6,22 @@ import org.junit.jupiter.api.Test;
 
 public class TestMain {
 
+    private static final String NUM_QUERIES = "1000";
+    private static final String SECONDS = "30";
+
     @Test
     public void testSQLite() {
         // do not check the result, since we still find bugs in the SQLite3 version included in the JDBC driver
-        Main.executeMain(new String[] { "--timeout-seconds", "30", "sqlite3", "--oracle", "NoREC" });
+        Main.executeMain(new String[] { "--timeout-seconds", SECONDS, "--num_queries", NUM_QUERIES, "sqlite3",
+                "--oracle", "NoREC" });
     }
 
     @Test
     public void testDuckDB() {
-        assertEquals(0, Main.executeMain(new String[] { "--timeout-seconds", "30", "duckdb", "--oracle", "NoREC" }));
+        assertEquals(0, Main.executeMain(new String[] { "--timeout-seconds", SECONDS, "--num_queries", NUM_QUERIES,
+                "duckdb", "--oracle", "NoREC" }));
+        assertEquals(0, Main.executeMain(new String[] { "--timeout-seconds", SECONDS, "--num_queries", NUM_QUERIES,
+                "duckdb", "--oracle", "QUERY_PARTITIONING" }));
     }
 
 }
