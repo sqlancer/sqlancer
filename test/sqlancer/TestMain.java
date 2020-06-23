daab779ce2da53fd55abf0ad57b996c62039d1f2
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2020-06-23 18:09:32 +0200

    Run DuckDB as part of the CI

--- test/sqlancer/TestMain.java
+++ test/sqlancer/TestMain.java
@@ -1,5 +1,20 @@
 package sqlancer;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class TestMain {
+
+    private static final String NUM_QUERIES = "1000";
+    private static final String SECONDS = "300";
+
+    @Test
+    public void testDuckDB() {
+        assertEquals(0, Main.executeMain(new String[] { "--timeout-seconds", SECONDS, "--num-queries", NUM_QUERIES,
+                "duckdb", "--oracle", "NoREC" }));
+        assertEquals(0, Main.executeMain(new String[] { "--timeout-seconds", SECONDS, "--num-queries", NUM_QUERIES,
+                "duckdb", "--oracle", "QUERY_PARTITIONING" }));
+    }
 
 }
