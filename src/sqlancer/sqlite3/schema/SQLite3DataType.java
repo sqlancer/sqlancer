68a0bcf69d63a1ab4d581c19acecd0b0b7b909c9
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2020-06-26 22:03:42 +0200

    [SQLite3] Remove unused method

--- src/sqlancer/sqlite3/schema/SQLite3DataType.java
+++ src/sqlancer/sqlite3/schema/SQLite3DataType.java
@@ -3,7 +3,4 @@ package sqlancer.sqlite3.schema;
 public enum SQLite3DataType {
     NULL, INT, TEXT, REAL, NONE, BINARY;
 
-    public boolean isNumeric() {
-        return this == REAL || this == INT;
-    }
 }
