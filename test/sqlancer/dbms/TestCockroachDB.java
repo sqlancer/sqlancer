f0a43b0a830d3f2765df47b7db4820ee770fbb65
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2020-06-26 14:06:27 +0200

    Integrate CockroachDB into the Travis CI gate

---
+++ test/sqlancer/dbms/TestCockroachDB.java
@@ -0,0 +1,21 @@
+package sqlancer.dbms;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import org.junit.jupiter.api.Test;
+
+import sqlancer.Main;
+
+public class TestCockroachDB {
+
+    @Test
+    public void testMySQL() {
+        String cockroachDB = System.getenv("COCKROACHDB_AVAILABLE");
+        boolean cockroachDBIsAvailable = cockroachDB != null && cockroachDB.equalsIgnoreCase("true");
+        assumeTrue(cockroachDBIsAvailable);
+        assertEquals(0, Main.executeMain(new String[] { "--timeout-seconds", TestConfig.SECONDS, "--num-queries",
+                TestConfig.NUM_QUERIES, "cockroachdb" }));
+    }
+
+}
