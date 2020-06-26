d1adcc0394e70990fff0326f5580ad88df065757
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2020-06-26 22:18:45 +0200

    [CockroachDB] Fix broken formatting

--- src/sqlancer/cockroachdb/gen/CockroachDBSetSessionGenerator.java
+++ src/sqlancer/cockroachdb/gen/CockroachDBSetSessionGenerator.java
@@ -21,18 +21,17 @@ public final class CockroachDBSetSessionGenerator {
 
     // https://www.cockroachlabs.com/docs/stable/set-vars.html
     private enum CockroachDBSetting {
-        BYTEA_OUTPUT((g) -> Randomly.fromOptions("hex", "escape", "base64")), DEFAULT_INT_SIZE(
-                (g) -> Randomly.fromOptions(4, 8)), DISTSQL(
-                        (g) -> Randomly.fromOptions("on", "off", "auto", "always")), ENABLE_IMPLICIT_SELECT_FOR_UPDATE(
-                                CockroachDBSetSessionGenerator::onOff), ENABLE_INSERT_FAST_PATH(
-                                        CockroachDBSetSessionGenerator::onOff), ENABLE_ZIGZAG_JOIN(
-                                                CockroachDBSetSessionGenerator::onOff),
+        BYTEA_OUTPUT((g) -> Randomly.fromOptions("hex", "escape", "base64")), //
+        DEFAULT_INT_SIZE((g) -> Randomly.fromOptions(4, 8)), //
+        DISTSQL((g) -> Randomly.fromOptions("on", "off", "auto", "always")), //
+        ENABLE_IMPLICIT_SELECT_FOR_UPDATE(CockroachDBSetSessionGenerator::onOff), //
+        ENABLE_INSERT_FAST_PATH(CockroachDBSetSessionGenerator::onOff), //
+        ENABLE_ZIGZAG_JOIN(CockroachDBSetSessionGenerator::onOff),
         // EXPERIMENTAL_ENABLE_HASH_SHARDED_INDEXES(CockroachDBSetSessionGenerator::onOff),
-        EXPERIMENTAL_SERIAL_NORMALIZATION(
-                (g) -> Randomly.fromOptions("'rowid'", "'virtual_sequence'")), EXTRA_FLOAT_DIGITS(
-                        (g) -> g.getRandomly().getInteger(-15, 3)), REORDER_JOINS_LIMIT(
-                                (g) -> g.getRandomly().getInteger(0, Integer.MAX_VALUE)), SQL_SAFE_UPDATES(
-                                        CockroachDBSetSessionGenerator::onOff),
+        EXPERIMENTAL_SERIAL_NORMALIZATION((g) -> Randomly.fromOptions("'rowid'", "'virtual_sequence'")), //
+        EXTRA_FLOAT_DIGITS((g) -> g.getRandomly().getInteger(-15, 3)), //
+        REORDER_JOINS_LIMIT((g) -> g.getRandomly().getInteger(0, Integer.MAX_VALUE)), //
+        SQL_SAFE_UPDATES(CockroachDBSetSessionGenerator::onOff),
         // TRACING(CockroachDBSetSessionGenerator::onOff)
         VECTORIZE((g) -> Randomly.fromOptions("auto", "on",
                 "off")); /*
