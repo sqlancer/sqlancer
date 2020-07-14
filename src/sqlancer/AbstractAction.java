6e85452c567d8a873e4f46a896b6995f478287d7
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2020-07-14 13:39:45 +0200

    Add a method to specify whether a db creation action should be retried

--- src/sqlancer/AbstractAction.java
+++ src/sqlancer/AbstractAction.java
@@ -6,4 +6,14 @@ public interface AbstractAction<G> {
 
     Query getQuery(G globalState) throws SQLException;
 
+    /**
+     * Specifies whether it makes sense to request a {@link Query}, when the previous call to {@link #getQuery(Object)}
+     * returned a query that failed executing.
+     *
+     * @return whether retrying getting queries makes sense, if the first query failed executing.
+     */
+    default boolean canBeRetried() {
+        return true;
+    }
+
 }
