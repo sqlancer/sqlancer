cfef87874bb7be3672de4af389002e12654f91b5
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2020-08-20 20:23:16 +0200

    [ClickHouse] Use the row value and tables abstractions

--- src/sqlancer/clickhouse/ClickHouseSchema.java
+++ src/sqlancer/clickhouse/ClickHouseSchema.java
@@ -6,21 +6,18 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import ru.yandex.clickhouse.domain.ClickHouseDataType;
-import sqlancer.IgnoreMeException;
 import sqlancer.Randomly;
-import sqlancer.StateToReproduce;
 import sqlancer.clickhouse.ClickHouseSchema.ClickHouseTable;
 import sqlancer.clickhouse.ast.ClickHouseConstant;
+import sqlancer.common.schema.AbstractRowValue;
 import sqlancer.common.schema.AbstractSchema;
 import sqlancer.common.schema.AbstractTable;
 import sqlancer.common.schema.AbstractTableColumn;
+import sqlancer.common.schema.AbstractTables;
 import sqlancer.common.schema.TableIndex;
 
 public class ClickHouseSchema extends AbstractSchema<ClickHouseTable> {
@@ -140,123 +137,20 @@ public class ClickHouseSchema extends AbstractSchema<ClickHouseTable> {
         return constant;
     }
 
-    public static class ClickHouseRowValue {
-        private final ClickHouseSchema.ClickHouseTables tables;
-        private final Map<ClickHouseSchema.ClickHouseColumn, ClickHouseConstant> values;
+    public static class ClickHouseRowValue
+            extends AbstractRowValue<ClickHouseTables, ClickHouseColumn, ClickHouseConstant> {
 
         ClickHouseRowValue(ClickHouseSchema.ClickHouseTables tables,
                 Map<ClickHouseSchema.ClickHouseColumn, ClickHouseConstant> values) {
-            this.tables = tables;
-            this.values = values;
-        }
-
-        public ClickHouseSchema.ClickHouseTables getTable() {
-            return tables;
-        }
-
-        public Map<ClickHouseSchema.ClickHouseColumn, ClickHouseConstant> getValues() {
-            return values;
-        }
-
-        @Override
-        public String toString() {
-            StringBuffer sb = new StringBuffer();
-            int i = 0;
-            for (ClickHouseSchema.ClickHouseColumn c : tables.getColumns()) {
-                if (i++ != 0) {
-                    sb.append(", ");
-                }
-                sb.append(values.get(c));
-            }
-            return sb.toString();
-        }
-
-        public String getRowValuesAsString() {
-            List<ClickHouseSchema.ClickHouseColumn> columnsToCheck = tables.getColumns();
-            return getRowValuesAsString(columnsToCheck);
-        }
-
-        public String getRowValuesAsString(List<ClickHouseSchema.ClickHouseColumn> columnsToCheck) {
-            StringBuilder sb = new StringBuilder();
-            Map<ClickHouseSchema.ClickHouseColumn, ClickHouseConstant> expectedValues = getValues();
-            for (int i = 0; i < columnsToCheck.size(); i++) {
-                if (i != 0) {
-                    sb.append(", ");
-                }
-                ClickHouseConstant expectedColumnValue = expectedValues.get(columnsToCheck.get(i));
-                ClickHouseToStringVisitor visitor = new ClickHouseToStringVisitor();
-                visitor.visit(expectedColumnValue);
-                sb.append(visitor.get());
-            }
-            return sb.toString();
+            super(tables, values);
         }
 
     }
 
-    public static class ClickHouseTables {
-        private final List<ClickHouseSchema.ClickHouseTable> tables;
-        private final List<ClickHouseSchema.ClickHouseColumn> columns;
+    public static class ClickHouseTables extends AbstractTables<ClickHouseTable, ClickHouseColumn> {
 
         public ClickHouseTables(List<ClickHouseSchema.ClickHouseTable> tables) {
-            this.tables = tables;
-            columns = new ArrayList<>();
-            for (ClickHouseSchema.ClickHouseTable t : tables) {
-                columns.addAll(t.getColumns());
-            }
-        }
-
-        public String tableNamesAsString() {
-            return tables.stream().map(t -> t.getName()).collect(Collectors.joining(", "));
-        }
-
-        public List<ClickHouseSchema.ClickHouseTable> getTables() {
-            return tables;
-        }
-
-        public List<ClickHouseSchema.ClickHouseColumn> getColumns() {
-            return columns;
-        }
-
-        public String columnNamesAsString() {
-            return getColumns().stream().map(t -> t.getTable().getName() + "." + t.getName())
-                    .collect(Collectors.joining(", "));
-        }
-
-        public String columnNamesAsString(Function<ClickHouseSchema.ClickHouseColumn, String> function) {
-            return getColumns().stream().map(function).collect(Collectors.joining(", "));
-        }
-
-        public ClickHouseRowValue getRandomRowValue(Connection con, StateToReproduce.ClickHouseStateToReproduce state)
-                throws SQLException {
-            String randomRow = String.format("SELECT %s, %s FROM %s ORDER BY RANDOM() LIMIT 1", columnNamesAsString(
-                    c -> c.getTable().getName() + "." + c.getName() + " AS " + c.getTable().getName() + c.getName()),
-                    columnNamesAsString(c -> "typeof(" + c.getTable().getName() + "." + c.getName() + ")"),
-                    tableNamesAsString());
-            Map<ClickHouseColumn, ClickHouseConstant> values = new HashMap<>();
-            try (Statement s = con.createStatement()) {
-                ResultSet randomRowValues;
-                try {
-                    randomRowValues = s.executeQuery(randomRow);
-                } catch (SQLException e) {
-                    throw new IgnoreMeException();
-                }
-                if (!randomRowValues.next()) {
-                    throw new AssertionError("could not find random row! " + randomRow + "\n" + state);
-                }
-                for (int i = 0; i < getColumns().size(); i++) {
-                    ClickHouseSchema.ClickHouseColumn column = getColumns().get(i);
-                    int columnIndex = randomRowValues.findColumn(column.getTable().getName() + column.getName());
-                    assert columnIndex == i + 1;
-                    String typeString = randomRowValues.getString(columnIndex + getColumns().size());
-                    ClickHouseDataType valueType = getColumnType(typeString).getType();
-                    ClickHouseConstant constant = getConstant(randomRowValues, columnIndex, valueType);
-                    values.put(column, constant);
-                }
-                assert !randomRowValues.next();
-                state.randomRowValues = values;
-                return new ClickHouseSchema.ClickHouseRowValue(this, values);
-            }
-
+            super(tables);
         }
 
     }
