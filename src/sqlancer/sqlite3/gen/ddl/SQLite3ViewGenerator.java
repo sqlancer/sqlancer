542bb527880a4b765e1ae351f28da751d33a4524
Manuel Rigger <manuel.rigger@inf.ethz.ch>
2020-08-21 09:53:56 +0200

    [SQLite] Do not use the PQS test generator for generating SELECTs for CREATE VIEW statements
    This functionality was initially added since the PQS test generator generated a larger subset of different SELECT statements.

--- src/sqlancer/sqlite3/gen/ddl/SQLite3ViewGenerator.java
+++ src/sqlancer/sqlite3/gen/ddl/SQLite3ViewGenerator.java
@@ -2,18 +2,15 @@ package sqlancer.sqlite3.gen.ddl;
 
 import java.sql.SQLException;
 
-import sqlancer.IgnoreMeException;
 import sqlancer.Randomly;
 import sqlancer.common.query.ExpectedErrors;
 import sqlancer.common.query.Query;
 import sqlancer.common.query.QueryAdapter;
+import sqlancer.sqlite3.SQLite3Errors;
 import sqlancer.sqlite3.SQLite3Provider.SQLite3GlobalState;
 import sqlancer.sqlite3.SQLite3Visitor;
 import sqlancer.sqlite3.ast.SQLite3Expression;
-import sqlancer.sqlite3.ast.SQLite3Select;
-import sqlancer.sqlite3.ast.SQLite3Select.SelectType;
 import sqlancer.sqlite3.gen.SQLite3Common;
-import sqlancer.sqlite3.oracle.SQLite3PivotedQuerySynthesisOracle;
 import sqlancer.sqlite3.oracle.SQLite3RandomQuerySynthesizer;
 import sqlancer.sqlite3.schema.SQLite3Schema;
 
@@ -42,38 +39,15 @@ public final class SQLite3ViewGenerator {
         }
         sb.append(SQLite3Common.getFreeViewName(globalState.getSchema()));
         ExpectedErrors errors = new ExpectedErrors();
+        SQLite3Errors.addExpectedExpressionErrors(errors);
         errors.add("is circularly defined");
         errors.add("unsupported frame specification");
         errors.add("The database file is locked");
-        if (Randomly.getBoolean()) {
-            SQLite3PivotedQuerySynthesisOracle queryGen = new SQLite3PivotedQuerySynthesisOracle(globalState);
-            try {
-                SQLite3Select q = queryGen.getQuery(globalState);
-                // for (SQLite3Expression expr : q.getFetchColumns()) {
-                // if (expr.getAffinity() != null || expr.getImplicitCollateSequence() != null ||
-                // expr.getExplicitCollateSequence() != null) {
-                // throw new IgnoreMeException();
-                // }
-                // }
-                if (!globalState.getDmbsSpecificOptions().testDistinctInView) {
-                    q.setSelectType(SelectType.ALL);
-                }
-                int size = q.getFetchColumns().size();
-                columnNamesAs(sb, size);
-                sb.append(SQLite3Visitor.asString(q));
-                SQLite3PivotedQuerySynthesisOracle.addExpectedErrors(errors);
-                return new QueryAdapter(sb.toString(), errors, true);
-            } catch (AssertionError e) {
-                throw new IgnoreMeException();
-            }
-        } else {
-            int size = 1 + Randomly.smallNumber();
-            columnNamesAs(sb, size);
-            SQLite3Expression randomQuery = SQLite3RandomQuerySynthesizer.generate(globalState, size);
-            sb.append(SQLite3Visitor.asString(randomQuery));
-            SQLite3PivotedQuerySynthesisOracle.addExpectedErrors(errors);
-            return new QueryAdapter(sb.toString(), errors, true);
-        }
+        int size = 1 + Randomly.smallNumber();
+        columnNamesAs(sb, size);
+        SQLite3Expression randomQuery = SQLite3RandomQuerySynthesizer.generate(globalState, size);
+        sb.append(SQLite3Visitor.asString(randomQuery));
+        return new QueryAdapter(sb.toString(), errors, true);
 
     }
 
