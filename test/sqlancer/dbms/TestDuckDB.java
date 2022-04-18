f43a0bebc8ae30b2da38d95d9e88f028dae3d606
Kok Rui <kokruiwong@gmail.com>
2022-04-18 20:42:54 +0800

    Re-enable QUERY_PARTITIONING oracle in CI for DuckDB

--- test/sqlancer/dbms/TestDuckDB.java
+++ test/sqlancer/dbms/TestDuckDB.java
@@ -11,8 +11,10 @@ public class TestDuckDB {
     @Test
     public void testDuckDB() {
         // run with one thread due to multithreading issues, see https://github.com/sqlancer/sqlancer/pull/45
-        assertEquals(0, Main.executeMain(new String[] { "--random-seed", "0", "--timeout-seconds", TestConfig.SECONDS,
-                "--num-threads", "1", "--num-queries", "0", "duckdb", "--oracle", "QUERY_PARTITIONING" }));
+        assertEquals(0,
+                Main.executeMain(new String[] { "--random-seed", "0", "--timeout-seconds", TestConfig.SECONDS,
+                        "--num-threads", "1", "--num-queries", TestConfig.NUM_QUERIES, "duckdb", "--oracle",
+                        "QUERY_PARTITIONING" }));
     }
 
 }
