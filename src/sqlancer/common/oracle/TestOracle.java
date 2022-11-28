efefa31b19e298dfd40d63e7525f85b63809e472
Kareem Shehata <kareem@shehata.ca>
2022-11-28 15:30:54 +0800

    Add Reproducer class and enable getting the reproducer from a TestOracle.

--- src/sqlancer/common/oracle/TestOracle.java
+++ src/sqlancer/common/oracle/TestOracle.java
@@ -1,9 +1,13 @@
 package sqlancer.common.oracle;
 
 import sqlancer.GlobalState;
+import sqlancer.Reproducer;
 
 public interface TestOracle<G extends GlobalState<?, ?, ?>> {
 
     void check() throws Exception;
 
+    default Reproducer<G> getLastReproducer() {
+        return null;
+    }
 }
