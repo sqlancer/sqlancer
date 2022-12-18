7272242bf476aaf9bd11394f57cc7e86e1e4c388
Nathee Jaywaree <puntf2@gmail.com>
2022-12-18 23:56:05 +0700

    Add AbstractUpdateGenerator class

---
+++ src/sqlancer/common/gen/AbstractUpdateGenerator.java
@@ -0,0 +1,24 @@
+package sqlancer.common.gen;
+
+import java.util.List;
+
+import sqlancer.common.schema.AbstractTableColumn;
+
+public abstract class AbstractUpdateGenerator<C extends AbstractTableColumn<?, ?>> {
+
+    protected StringBuilder sb = new StringBuilder();
+
+    protected void updateColumns(List<C> columns) {
+        for (int nrColumn = 0; nrColumn < columns.size(); nrColumn++) {
+            if (nrColumn != 0) {
+                sb.append(", ");
+            }
+            sb.append(columns.get(nrColumn).getName());
+            sb.append("=");
+            updateValue(columns.get(nrColumn));
+        }
+    }
+
+    protected abstract void updateValue(C column);
+
+}
