f4cee7e58f670ecade1836a0d4b153a517d97248
bajinsheng <43855029@qq.com>
2022-12-18 17:08:25 +0800

    opt: action update tidb

--- src/sqlancer/tidb/TiDBProvider.java
+++ src/sqlancer/tidb/TiDBProvider.java
@@ -16,16 +16,16 @@ import sqlancer.SQLConnection;
 import sqlancer.SQLGlobalState;
 import sqlancer.SQLProviderAdapter;
 import sqlancer.StatementExecutor;
-import sqlancer.common.query.ExpectedErrors;
 import sqlancer.common.query.SQLQueryAdapter;
 import sqlancer.common.query.SQLQueryProvider;
 import sqlancer.tidb.TiDBProvider.TiDBGlobalState;
 import sqlancer.tidb.gen.TiDBAlterTableGenerator;
 import sqlancer.tidb.gen.TiDBAnalyzeTableGenerator;
 import sqlancer.tidb.gen.TiDBDeleteGenerator;
+import sqlancer.tidb.gen.TiDBDropTableGenerator;
+import sqlancer.tidb.gen.TiDBDropViewGenerator;
 import sqlancer.tidb.gen.TiDBIndexGenerator;
 import sqlancer.tidb.gen.TiDBInsertGenerator;
-import sqlancer.tidb.gen.TiDBRandomQuerySynthesizer;
 import sqlancer.tidb.gen.TiDBSetGenerator;
 import sqlancer.tidb.gen.TiDBTableGenerator;
 import sqlancer.tidb.gen.TiDBUpdateGenerator;
@@ -39,25 +39,20 @@ public class TiDBProvider extends SQLProviderAdapter<TiDBGlobalState, TiDBOption
     }
 
     public enum Action implements AbstractAction<TiDBGlobalState> {
-        INSERT(TiDBInsertGenerator::getQuery), //
-        ANALYZE_TABLE(TiDBAnalyzeTableGenerator::getQuery), //
-        TRUNCATE((g) -> new SQLQueryAdapter("TRUNCATE " + g.getSchema().getRandomTable(t -> !t.isView()).getName())), //
-        CREATE_INDEX(TiDBIndexGenerator::getQuery), //
-        DELETE(TiDBDeleteGenerator::getQuery), //
-        SET(TiDBSetGenerator::getQuery), //
-        UPDATE(TiDBUpdateGenerator::getQuery), //
+        CREATE_TABLE(TiDBTableGenerator::createRandomTableStatement), // 0
+        CREATE_INDEX(TiDBIndexGenerator::getQuery), // 1
+        VIEW_GENERATOR(TiDBViewGenerator::getQuery), // 2
+        INSERT(TiDBInsertGenerator::getQuery), // 3
+        ALTER_TABLE(TiDBAlterTableGenerator::getQuery), // 4
+        TRUNCATE((g) -> new SQLQueryAdapter("TRUNCATE " + g.getSchema().getRandomTable(t -> !t.isView()).getName())), // 5
+        UPDATE(TiDBUpdateGenerator::getQuery), // 6
+        DELETE(TiDBDeleteGenerator::getQuery), // 7
+        SET(TiDBSetGenerator::getQuery), // 8
         ADMIN_CHECKSUM_TABLE(
-                (g) -> new SQLQueryAdapter("ADMIN CHECKSUM TABLE " + g.getSchema().getRandomTable().getName())), //
-        VIEW_GENERATOR(TiDBViewGenerator::getQuery), //
-        ALTER_TABLE(TiDBAlterTableGenerator::getQuery), //
-        EXPLAIN((g) -> {
-            ExpectedErrors errors = new ExpectedErrors();
-            TiDBErrors.addExpressionErrors(errors);
-            TiDBErrors.addExpressionHavingErrors(errors);
-            return new SQLQueryAdapter(
-                    "EXPLAIN " + TiDBRandomQuerySynthesizer.generate(g, Randomly.smallNumber() + 1).getQueryString(),
-                    errors);
-        });
+                (g) -> new SQLQueryAdapter("ADMIN CHECKSUM TABLE " + g.getSchema().getRandomTable().getName())), // 9
+        ANALYZE_TABLE(TiDBAnalyzeTableGenerator::getQuery), // 10
+        DROP_TABLE(TiDBDropTableGenerator::dropTable), // 11
+        DROP_VIEW(TiDBDropViewGenerator::dropView); // 12
 
         private final SQLQueryProvider<TiDBGlobalState> sqlQueryProvider;
 
@@ -87,8 +82,6 @@ public class TiDBProvider extends SQLProviderAdapter<TiDBGlobalState, TiDBOption
         case CREATE_INDEX:
             return r.getInteger(0, 2);
         case INSERT:
-        case EXPLAIN:
-            return r.getInteger(0, globalState.getOptions().getMaxNumberInserts());
         case TRUNCATE:
         case DELETE:
         case ADMIN_CHECKSUM_TABLE:
@@ -101,6 +94,10 @@ public class TiDBProvider extends SQLProviderAdapter<TiDBGlobalState, TiDBOption
             return r.getInteger(0, 2);
         case ALTER_TABLE:
             return r.getInteger(0, 10); // https://github.com/tidb-challenge-program/bug-hunting-issue/issues/10
+        case CREATE_TABLE:
+        case DROP_TABLE:
+        case DROP_VIEW:
+            return 0;
         default:
             throw new AssertionError(a);
         }
