63e5f31afeeeda5824995b6a6160cf7922fd1992
ZuoTiJia <2239651886@qq.com>
2023-01-19 13:53:17 +0800

    cnosdb: fix compiler error

--- src/sqlancer/cnosdb/client/CnosDBException.java
+++ src/sqlancer/cnosdb/client/CnosDBException.java
@@ -1,6 +1,8 @@
 package sqlancer.cnosdb.client;
 
 public class CnosDBException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     CnosDBException(String message) {
         super(message);
     }
