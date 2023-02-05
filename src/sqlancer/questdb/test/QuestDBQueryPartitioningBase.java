2557514fe0f9ad7e49b3b4fb805f94113b35c8c5
Ming Cheng <mcheng991129@gmail.com>
2023-02-05 16:07:44 -0500

    Forbid multiple table selection from QuestDB

--- src/sqlancer/questdb/test/QuestDBQueryPartitioningBase.java
+++ src/sqlancer/questdb/test/QuestDBQueryPartitioningBase.java
@@ -29,6 +29,7 @@ public class QuestDBQueryPartitioningBase
 
     QuestDBSchema s;
     QuestDBTables targetTables;
+    QuestDBTable targetTable; // single table
     QuestDBExpressionGenerator gen;
     QuestDBSelect select;
 
@@ -42,7 +43,7 @@ public class QuestDBQueryPartitioningBase
         if (Randomly.getBoolean()) {
             columns.add(new ColumnReferenceNode<>(new QuestDBColumn("*", null, false)));
         } else {
-            columns = Randomly.nonEmptySubset(targetTables.getColumns()).stream()
+            columns = Randomly.nonEmptySubset(targetTable.getColumns()).stream()
                     .map(c -> new ColumnReferenceNode<QuestDBExpression, QuestDBColumn>(c))
                     .collect(Collectors.toList());
         }
@@ -66,12 +67,15 @@ public class QuestDBQueryPartitioningBase
     @Override
     public void check() throws SQLException {
         s = state.getSchema();
-        targetTables = s.getRandomTableNonEmptyTables();
-        gen = new QuestDBExpressionGenerator(state).setColumns(targetTables.getColumns());
+        // Only return one table instead of multiple tables, which is regarded as illegal by QuestDB 
+        // e.g. "SELECT * FROM t0, t1;"
+        targetTable = s.getRandomTable(t -> !t.isView()); 
+        gen = new QuestDBExpressionGenerator(state).setColumns(targetTable.getColumns()); 
         initializeTernaryPredicateVariants();
         select = new QuestDBSelect();
         select.setFetchColumns(generateFetchColumns());
-        List<QuestDBTable> tables = targetTables.getTables();
+        List<QuestDBTable> tables = new ArrayList<>(); 
+        tables.add(targetTable); 
         List<TableReferenceNode<QuestDBExpression, QuestDBTable>> tableList = tables.stream()
                 .map(t -> new TableReferenceNode<QuestDBExpression, QuestDBTable>(t)).collect(Collectors.toList());
         // Ignore JOINs for now
