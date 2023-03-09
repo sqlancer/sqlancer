c1e40c46cb3df77392e67d948f398c9e377ea6f8
ZhengLin Li <1125806272@qq.com>
2023-03-09 19:39:25 +0800

    fix: remove expected errors timesaledb

--- src/sqlancer/timescaledb/gen/TimescaleDBCommon.java
+++ src/sqlancer/timescaledb/gen/TimescaleDBCommon.java
@@ -8,10 +8,6 @@ public final class TimescaleDBCommon {
     }
 
     public static void addTimescaleDBErrors(ExpectedErrors errors) {
-        errors.add("Error updating TimescaleDB when using a third-party PostgreSQL administration tool");
-        errors.add("Log error: could not access file \"timescaledb\"");
-        errors.add("ERROR: could not access file \"timescaledb-\\<version>\": No such file or directory");
-        errors.add("Scheduled jobs stop running");
-        errors.add("Failed to start a background worker");
+
     }
 }
