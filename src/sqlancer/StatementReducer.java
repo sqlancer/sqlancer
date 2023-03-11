49ef4deb548c57d40003311986886bf472fbb641
ColinYoungTaro <colin.young.taro@gmail.com>
2023-03-11 11:57:57 +0800

    feat: Implement delta-debugging as the reduction strategy

--- src/sqlancer/StatementReducer.java
+++ src/sqlancer/StatementReducer.java
@@ -2,7 +2,6 @@ package sqlancer;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 import sqlancer.common.query.Query;
@@ -11,9 +10,15 @@ public class StatementReducer<G extends GlobalState<O, ?, C>, O extends DBMSSpec
         implements Reducer<G> {
     private final DatabaseProvider<G, O, C> provider;
     private boolean observedChange;
+    private int partitionNum;
 
     public StatementReducer(DatabaseProvider<G, O, C> provider) {
         this.provider = provider;
+    }
+
+    @FunctionalInterface
+    private interface TriFunction<A, B, C, R> {
+        R apply(A a, B b, C c);
     }
 
     @SuppressWarnings("unchecked")
@@ -28,14 +33,28 @@ public class StatementReducer<G extends GlobalState<O, ?, C>, O extends DBMSSpec
         printQueries(knownToReproduceBugStatements);
         System.out.println();
 
-        do {
+        if (knownToReproduceBugStatements.size() <= 1) {
+            return;
+        }
+
+        partitionNum = 2;
+        while (knownToReproduceBugStatements.size() >= 2) {
             observedChange = false;
+
             knownToReproduceBugStatements = tryReduction(state, reproducer, newGlobalState,
-                    knownToReproduceBugStatements, (candidateStatements, i) -> {
-                        candidateStatements.remove((int) i);
+                    knownToReproduceBugStatements, (candidateStatements, s, l) -> {
+                        List<Query<C>> remainedStatements = new ArrayList<>();
+                        candidateStatements.subList(s, s+l).clear();
                         return true;
                     });
-        } while (observedChange);
+            if (!observedChange) {
+                if (partitionNum == knownToReproduceBugStatements.size()) {
+                    break;
+                }
+                // increase the search granularity
+                partitionNum = Math.min(partitionNum * 2, knownToReproduceBugStatements.size());
+            }
+        }
 
         System.out.println("Reduced query:");
         printQueries(knownToReproduceBugStatements);
@@ -43,17 +62,24 @@ public class StatementReducer<G extends GlobalState<O, ?, C>, O extends DBMSSpec
 
     private List<Query<C>> tryReduction(G state, // NOPMD
             Reproducer<G> reproducer, G newGlobalState, List<Query<C>> knownToReproduceBugStatements,
-            BiFunction<List<Query<C>>, Integer, Boolean> reductionOperation) throws Exception {
+            TriFunction<List<Query<C>>, Integer, Integer, Boolean> reductionOperation) throws Exception {
 
         List<Query<C>> statements = knownToReproduceBugStatements;
-        for (int i = 0; i < statements.size(); i++) {
+
+        int start = 0;
+        int subLength = statements.size() / partitionNum;
+        while (start < statements.size()) {
+            // newStatements = candidate[:start] + candidate[start+subLength:]
+            // in other word, remove [start, start+subLength) from candidates
             try (C con2 = provider.createDatabase(newGlobalState)) {
                 newGlobalState.setConnection(con2);
                 List<Query<C>> candidateStatements = new ArrayList<>(statements);
-                if (!reductionOperation.apply(candidateStatements, i)) {
+                if (!reductionOperation.apply(candidateStatements, start, subLength)) {
+                    start = start + subLength;
                     continue;
                 }
-                newGlobalState.getState().setStatements(candidateStatements.stream().collect(Collectors.toList()));
+                newGlobalState.getState().setStatements(new ArrayList<>(candidateStatements));
+
                 for (Query<C> s : candidateStatements) {
                     try {
                         s.execute(newGlobalState);
@@ -65,6 +91,8 @@ public class StatementReducer<G extends GlobalState<O, ?, C>, O extends DBMSSpec
                     if (reproducer.bugStillTriggers(newGlobalState)) {
                         observedChange = true;
                         statements = candidateStatements;
+                        partitionNum = Math.max(partitionNum - 1, 2);
+                        break;
                         // reproducer.outputHook((SQLite3GlobalState) newGlobalState);
                         // state.getLogger().logReduced(newGlobalState.getState());
                     }
@@ -72,6 +100,7 @@ public class StatementReducer<G extends GlobalState<O, ?, C>, O extends DBMSSpec
 
                 }
             }
+            start = start + subLength;
         }
         return statements;
     }
