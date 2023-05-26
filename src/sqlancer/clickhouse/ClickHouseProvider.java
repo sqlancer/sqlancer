5d15340329e597c5c9e94d427c7b39bc1a019914
Ilya Yatsishin <2159081+qoega@users.noreply.github.com>
2023-05-26 16:06:40 +0200

    It works locally

--- src/sqlancer/clickhouse/ClickHouseProvider.java
+++ src/sqlancer/clickhouse/ClickHouseProvider.java
@@ -143,7 +143,7 @@ public class ClickHouseProvider extends SQLProviderAdapter<ClickHouseGlobalState
         }
         con.close();
         con = DriverManager.getConnection(
-                String.format("jdbc:clickhouse://%s:%d/%s?allow_suspicious_indices=1&socket_timeout=300000%s%s", host,
+                String.format("jdbc:clickhouse://%s:%d/%s?socket_timeout=300000&allow_suspicious_indices=1%s", host,
                         port, databaseName, clickHouseOptions.enableAnalyzer ? "&allow_experimental_analyzer=1" : ""),
                 globalState.getOptions().getUserName(), globalState.getOptions().getPassword());
         return new SQLConnection(con);
