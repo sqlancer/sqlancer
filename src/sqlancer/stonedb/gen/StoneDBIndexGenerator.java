13487860875f219c8fbc8549a573f433064d3086
zhenglinli <1125806272@qq.com>
2023-06-30 14:20:00 +0800

    fix: Tianmu engine does not support fulltext index.

--- src/sqlancer/stonedb/gen/StoneDBIndexGenerator.java
+++ src/sqlancer/stonedb/gen/StoneDBIndexGenerator.java
@@ -24,7 +24,8 @@ public class StoneDBIndexGenerator {
 
     private SQLQueryAdapter getQuery() {
         sb.append("CREATE ");
-        sb.append(Randomly.fromOptions("UNIQUE", "FULLTEXT", "SPATIAL"));
+        // Tianmu engine does not support fulltext index.
+        sb.append(Randomly.fromOptions("UNIQUE" /* "FULLTEXT" */, "SPATIAL"));
         sb.append(" INDEX ");
         sb.append(globalState.getSchema().getFreeIndexName());
         appendIndexType();
