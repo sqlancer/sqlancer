da951160c1816e2f21b35ff15701f6b4de7d2030
zhenglinli <1125806272@qq.com>
2023-07-07 08:32:00 +0800

    refactor: refactor append type, reused function

--- src/sqlancer/stonedb/gen/StoneDBTableAlterGenerator.java
+++ src/sqlancer/stonedb/gen/StoneDBTableAlterGenerator.java
@@ -1,16 +1,20 @@
 package sqlancer.stonedb.gen;
 
+import java.util.List;
+
 import sqlancer.Randomly;
 import sqlancer.common.query.ExpectedErrors;
 import sqlancer.common.query.SQLQueryAdapter;
 import sqlancer.stonedb.StoneDBProvider.StoneDBGlobalState;
 import sqlancer.stonedb.StoneDBSchema.StoneDBColumn;
 import sqlancer.stonedb.StoneDBSchema.StoneDBCompositeDataType;
+import sqlancer.stonedb.StoneDBSchema.StoneDBDataType;
 import sqlancer.stonedb.StoneDBSchema.StoneDBTable;
 
 public class StoneDBTableAlterGenerator {
     private final StoneDBGlobalState globalState;
     private final StringBuilder sb = new StringBuilder();
+    private final StoneDBTable table;
     ExpectedErrors errors = new ExpectedErrors();
 
     enum Action {
@@ -19,6 +23,7 @@ public class StoneDBTableAlterGenerator {
 
     public StoneDBTableAlterGenerator(StoneDBGlobalState globalState) {
         this.globalState = globalState;
+        table = globalState.getSchema().getRandomTable(t -> !t.isView());
     }
 
     public static SQLQueryAdapter generate(StoneDBGlobalState globalState) {
@@ -27,12 +32,33 @@ public class StoneDBTableAlterGenerator {
 
     private SQLQueryAdapter getQuery() {
         sb.append("ALTER TABLE ");
-        StoneDBTable table = globalState.getSchema().getRandomTable(t -> !t.isView());
-        StoneDBExpressionGenerator generator = new StoneDBExpressionGenerator(globalState)
-                .setColumns(table.getColumns());
         sb.append(table.getName());
         sb.append(" ");
-        Action action = Randomly.fromOptions(Action.values());
+        appendAlterOptions();
+        addExpectedErrors();
+        return new SQLQueryAdapter(sb.toString(), errors, true);
+    }
+
+    private void addExpectedErrors() {
+        // java.sql.SQLSyntaxErrorException: You can't delete all columns with ALTER TABLE; use DROP TABLE instead
+        errors.add("You can't delete all columns with ALTER TABLE; use DROP TABLE instead");
+    }
+
+    private void appendAlterOptions() {
+        List<Action> actions;
+        if (Randomly.getBooleanWithSmallProbability()) {
+            actions = Randomly.subset(Action.values());
+        } else {
+            actions = List.of(Randomly.fromOptions(Action.values()));
+        }
+        for (Action action : actions) {
+            appendAlterOption(action);
+        }
+    }
+
+    private void appendAlterOption(Action action) {
+        StoneDBExpressionGenerator generator = new StoneDBExpressionGenerator(globalState)
+                .setColumns(table.getColumns());
         switch (action) {
         case ADD_COLUMN:
             sb.append("ADD COLUMN ");
@@ -54,7 +80,7 @@ public class StoneDBTableAlterGenerator {
             sb.append(table.getRandomColumn().getName());
             break;
         case ALTER_COLUMN:
-            sb.append(Randomly.fromOptions("ALTER COLUMN ", "ALTER "));
+            sb.append(Randomly.fromOptions("CHANGE COLUMN ", "CHANGE "));
             StoneDBColumn randomColumn = table.getRandomColumn();
             sb.append(randomColumn.getName());
             if (Randomly.getBoolean()) {
@@ -63,17 +89,13 @@ public class StoneDBTableAlterGenerator {
             } else {
                 sb.append(" DROP DEFAULT");
             }
-            if (Randomly.getBoolean()) {
-                sb.append(" SET ").append(Randomly.fromOptions("VISIBLE", "INVISIBLE"));
-            }
             break;
         case CHANGE_COLUMN:
             sb.append(Randomly.fromOptions("CHANGE COLUMN ", "CHANGE "));
             String oldColumnName = table.getRandomColumn().getName();
             String newColumnName = table.getFreeColumnName();
-            sb.append(oldColumnName).append(" ").append(newColumnName);
-            sb.append(" ");
-            sb.append(StoneDBCompositeDataType.getRandomWithoutNull().getPrimitiveDataType().toString());
+            sb.append(oldColumnName).append(" ").append(newColumnName).append(" ");
+            StoneDBDataType.appendTypeAndValue(StoneDBDataType.getRandomWithoutNull(), sb, globalState.getRandomly());
             if (Randomly.getBoolean()) {
                 if (Randomly.getBoolean()) {
                     sb.append(" FIRST");
@@ -86,18 +108,11 @@ public class StoneDBTableAlterGenerator {
         case RENAME_COLUMN:
             sb.append("RENAME COLUMN ");
             sb.append(table.getRandomColumn().getName());
-            sb.append(" TO ");
+            sb.append(Randomly.fromOptions(" TO ", " AS "));
             sb.append(table.getFreeColumnName());
             break;
         default:
             throw new AssertionError(action);
         }
-        addExpectedErrors();
-        return new SQLQueryAdapter(sb.toString(), errors, true);
-    }
-
-    private void addExpectedErrors() {
-        // java.sql.SQLSyntaxErrorException: You can't delete all columns with ALTER TABLE; use DROP TABLE instead
-        errors.add("You can't delete all columns with ALTER TABLE; use DROP TABLE instead");
     }
 }
