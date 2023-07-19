a9ec3df837974f003495622976368513c329d70b
zhenglinli <1125806272@qq.com>
2023-07-19 11:43:59 +0800

    feat: add expected expression error

--- src/sqlancer/stonedb/StoneDBErrors.java
+++ src/sqlancer/stonedb/StoneDBErrors.java
@@ -9,6 +9,8 @@ public final class StoneDBErrors {
     public static void addExpectedExpressionErrors(ExpectedErrors errors) {
         // java.sql.SQLException: Incorrect DATE value: '292269055-12-02'
         errors.add("Incorrect DATE value: ");
+        // java.sql.SQLException: Incorrect string value: '\xBC\xE7\xC9\x91\x05R...' for column 'c1' at row 1
+        errors.add("Incorrect string value: ");
     }
 
 }
