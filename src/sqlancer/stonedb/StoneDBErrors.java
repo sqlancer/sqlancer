a84170a9d4ba27460f32d138f0407c88e3b1e302
zhenglinli <1125806272@qq.com>
2023-07-30 15:01:52 +0800

    fix: delete useless expected errors

--- src/sqlancer/stonedb/StoneDBErrors.java
+++ src/sqlancer/stonedb/StoneDBErrors.java
@@ -7,8 +7,6 @@ public final class StoneDBErrors {
     }
 
     public static void addExpectedExpressionErrors(ExpectedErrors errors) {
-        // java.sql.SQLException: Incorrect DATETIME value: '292269055-12-02 16:47:04'
-        errors.add("Incorrect DATETIME value: ");
         // java.sql.SQLException: Incorrect DATE value: '292269055-12-02'
         errors.add("Incorrect DATE value: ");
         // java.sql.SQLException: Incorrect string value: '\xBC\xE7\xC9\x91\x05R...' for column 'c1' at row 1
