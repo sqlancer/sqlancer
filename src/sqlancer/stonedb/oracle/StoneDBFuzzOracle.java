31841b872a12f82463f212136167d1893d4cafe8
zhenglinli <1125806272@qq.com>
2023-08-12 07:56:11 +0800

    feat: generate random columns

--- src/sqlancer/stonedb/oracle/StoneDBFuzzOracle.java
+++ src/sqlancer/stonedb/oracle/StoneDBFuzzOracle.java
@@ -6,8 +6,6 @@ import java.util.stream.Collectors;
 
 import sqlancer.Randomly;
 import sqlancer.common.ast.newast.ColumnReferenceNode;
-import sqlancer.common.ast.newast.NewBinaryOperatorNode;
-import sqlancer.common.ast.newast.NewPostfixTextNode;
 import sqlancer.common.ast.newast.Node;
 import sqlancer.common.ast.newast.TableReferenceNode;
 import sqlancer.common.oracle.TestOracle;
@@ -17,7 +15,6 @@ import sqlancer.stonedb.StoneDBErrors;
 import sqlancer.stonedb.StoneDBProvider.StoneDBGlobalState;
 import sqlancer.stonedb.StoneDBSchema;
 import sqlancer.stonedb.StoneDBSchema.StoneDBColumn;
-import sqlancer.stonedb.StoneDBSchema.StoneDBDataType;
 import sqlancer.stonedb.StoneDBSchema.StoneDBTable;
 import sqlancer.stonedb.StoneDBSchema.StoneDBTables;
 import sqlancer.stonedb.StoneDBToStringVisitor;
@@ -25,8 +22,6 @@ import sqlancer.stonedb.ast.StoneDBExpression;
 import sqlancer.stonedb.ast.StoneDBJoin;
 import sqlancer.stonedb.ast.StoneDBSelect;
 import sqlancer.stonedb.gen.StoneDBExpressionGenerator;
-import sqlancer.stonedb.gen.StoneDBExpressionGenerator.StoneDBBinaryLogicalOperator;
-import sqlancer.stonedb.gen.StoneDBExpressionGenerator.StoneDBCastOperation;
 
 public class StoneDBFuzzOracle implements TestOracle<StoneDBGlobalState> {
     private final StoneDBGlobalState globalState;
@@ -48,25 +43,21 @@ public class StoneDBFuzzOracle implements TestOracle<StoneDBGlobalState> {
         List<StoneDBTable> tables = randomTables.getTables();
         List<TableReferenceNode<StoneDBExpression, StoneDBTable>> tableList = tables.stream()
                 .map(t -> new TableReferenceNode<StoneDBExpression, StoneDBTable>(t)).collect(Collectors.toList());
+        List<Node<StoneDBExpression>> allColumns = columns.stream()
+                .map((c) -> new ColumnReferenceNode<StoneDBExpression, StoneDBColumn>(c)).collect(Collectors.toList());
         List<Node<StoneDBExpression>> joins = StoneDBJoin.getJoins(tableList, globalState);
 
         StoneDBSelect select = new StoneDBSelect();
-        Node<StoneDBExpression> asText = new NewPostfixTextNode<>(
-                new StoneDBCastOperation(
-                        new NewBinaryOperatorNode<>(new NewPostfixTextNode<>(randomWhereCondition, " IS NOT NULL "),
-                                randomWhereCondition, StoneDBBinaryLogicalOperator.AND),
-                        StoneDBDataType.INT),
-                " as count");
-        select.setFetchColumns(List.of(asText));
+        select.setFetchColumns(
+                Randomly.fromOptions(List.of(new ColumnReferenceNode<>(new StoneDBColumn("*", null, false, false, 0))),
+                        allColumns, Randomly.subset(allColumns)));
         select.setFromList(new ArrayList<>(tableList));
         select.setJoinList(joins);
-        String queryString = "SELECT * FROM (" + StoneDBToStringVisitor.asString(select) + ") as res;";
+        String queryString = StoneDBToStringVisitor.asString(select);
         SQLQueryAdapter q = new SQLQueryAdapter(queryString, errors);
         q.executeAndGetLogged(globalState);
 
         select = new StoneDBSelect();
-        List<Node<StoneDBExpression>> allColumns = columns.stream()
-                .map((c) -> new ColumnReferenceNode<StoneDBExpression, StoneDBColumn>(c)).collect(Collectors.toList());
         select.setFetchColumns(allColumns);
         select.setFromList(new ArrayList<>(tableList));
         select.setWhereClause(randomWhereCondition);
