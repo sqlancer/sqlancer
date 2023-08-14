33d69573cb150197f836f87e76fe7d79f1f6d269
ZHENGLIN LI <63448884+ZhengLin-Li@users.noreply.github.com>
2023-08-14 12:07:50 +0800

    feat: bypass logical XOR (#877)
    * feat: bypass logical XOR
    
    * fix: change import order
    
    * fix: fix style check error

--- src/sqlancer/stonedb/StoneDBBugs.java
+++ src/sqlancer/stonedb/StoneDBBugs.java
@@ -39,6 +39,12 @@ public final class StoneDBBugs {
     // INSERT INTO t0(c0) VALUE ('16');
     // SELECT t0.c0 FROM t0 WHERE t0.c0; -- expected 1 row but got 2 rows
     public static boolean bugNotReported7 = true;
+    // CREATE TABLE t0(c0 INT) ;
+    // SELECT * FROM t0 WHERE ((t0.c0)XOR((t0.c0)));
+    // ERROR 6 (HY000): The query includes syntax that is not supported by the storage engine. Either restructure the
+    // query with supported syntax, or enable the MySQL core::Query Path in config file to execute the query with
+    // reduced performance.
+    public static boolean bugNotReportedXOR = true;
 
     private StoneDBBugs() {
     }
