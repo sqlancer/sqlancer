0d6ba67c92d2a844fb9c4adb713655f9e5934ae1
Aryan Patil <95161886+AryanP45@users.noreply.github.com>
2024-01-08 22:00:48 +0530

    Typo in testH2 (#905)

--- test/sqlancer/dbms/TestH2.java
+++ test/sqlancer/dbms/TestH2.java
@@ -10,10 +10,10 @@ import sqlancer.Main;
 public class TestH2 {
 
     @Test
-    public void testDuckDB() {
+    public void testH2DB() {
         String h2Available = System.getenv("H2_AVAILABLE");
-        boolean mariaDBIsAvailable = h2Available != null && h2Available.equalsIgnoreCase("true");
-        assumeTrue(mariaDBIsAvailable);
+        boolean h2DBIsAvailable = h2Available != null && h2Available.equalsIgnoreCase("true");
+        assumeTrue(h2DBIsAvailable);
         assertEquals(0, Main.executeMain(new String[] { "--random-seed", "0", "--timeout-seconds", TestConfig.SECONDS,
                 "--num-threads", "4", "--num-queries", TestConfig.NUM_QUERIES, "h2" }));
 
