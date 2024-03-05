21976f9f2ea791d45dee5c803a22d521da8d987a
malwaregarry <malwaregarry@gmail.com>
2024-03-05 13:24:45 +0800

    Implement generic TLP group-by oracle

--- src/sqlancer/sqlite3/oracle/tlp/SQLite3TLPGroupByOracle.java
+++ src/sqlancer/sqlite3/oracle/tlp/SQLite3TLPGroupByOracle.java
@@ -1,56 +1,38 @@
 package sqlancer.sqlite3.oracle.tlp;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
-import sqlancer.ComparatorHelper;
-import sqlancer.Randomly;
+import sqlancer.common.oracle.TLPGroupByOracle;
+import sqlancer.common.oracle.TestOracle;
+import sqlancer.common.query.ExpectedErrors;
+import sqlancer.sqlite3.SQLite3Errors;
 import sqlancer.sqlite3.SQLite3GlobalState;
-import sqlancer.sqlite3.SQLite3Visitor;
 import sqlancer.sqlite3.ast.SQLite3Expression;
-import sqlancer.sqlite3.ast.SQLite3Expression.SQLite3ColumnName;
+import sqlancer.sqlite3.ast.SQLite3Expression.Join;
+import sqlancer.sqlite3.gen.SQLite3ExpressionGenerator;
+import sqlancer.sqlite3.schema.SQLite3Schema;
+import sqlancer.sqlite3.schema.SQLite3Schema.SQLite3Column;
+import sqlancer.sqlite3.schema.SQLite3Schema.SQLite3Table;
 
-public class SQLite3TLPGroupByOracle extends SQLite3TLPBase {
+public class SQLite3TLPGroupByOracle implements TestOracle<SQLite3GlobalState> {
 
-    private String generatedQueryString;
+    TLPGroupByOracle<Join, SQLite3Expression, SQLite3Schema, SQLite3Table, SQLite3Column, SQLite3GlobalState> oracle;
 
     public SQLite3TLPGroupByOracle(SQLite3GlobalState state) {
-        super(state);
+        SQLite3ExpressionGenerator gen = new SQLite3ExpressionGenerator(state);
+        ExpectedErrors expectedErrors = ExpectedErrors.newErrors()
+                .with(SQLite3Errors.getExpectedExpressionErrors().toArray(new String[0])).build();
+        oracle = new TLPGroupByOracle<>(state, gen, expectedErrors);
     }
 
     @Override
     public void check() throws SQLException {
-        super.check();
-        select.setGroupByClause(select.getFetchColumns());
-        select.setWhereClause(null);
-        String originalQueryString = SQLite3Visitor.asString(select);
-        generatedQueryString = originalQueryString;
-        List<String> resultSet = ComparatorHelper.getResultSetFirstColumnAsString(originalQueryString, errors, state);
-
-        select.setWhereClause(predicate);
-        String firstQueryString = SQLite3Visitor.asString(select);
-        select.setWhereClause(negatedPredicate);
-        String secondQueryString = SQLite3Visitor.asString(select);
-        select.setWhereClause(isNullPredicate);
-        String thirdQueryString = SQLite3Visitor.asString(select);
-        List<String> combinedString = new ArrayList<>();
-        List<String> secondResultSet = ComparatorHelper.getCombinedResultSetNoDuplicates(firstQueryString,
-                secondQueryString, thirdQueryString, combinedString, true, state, errors);
-        ComparatorHelper.assumeResultSetsAreEqual(resultSet, secondResultSet, originalQueryString, combinedString,
-                state);
-    }
-
-    @Override
-    List<SQLite3Expression> generateFetchColumns() {
-        return Randomly.nonEmptySubset(targetTables.getColumns()).stream().map(c -> new SQLite3ColumnName(c, null))
-                .collect(Collectors.toList());
+        oracle.check();
     }
 
     @Override
     public String getLastQueryString() {
-        return generatedQueryString;
+        return oracle.getLastQueryString();
     }
 
 }
