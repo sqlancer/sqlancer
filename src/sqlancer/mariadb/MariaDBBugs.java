094756668c151084cf567e401aa8cca83370555d
Jinsheng Ba <43855029@qq.com>
2024-03-19 21:20:55 +0800

    MariaDB DQP (#925)

--- src/sqlancer/mariadb/MariaDBBugs.java
+++ src/sqlancer/mariadb/MariaDBBugs.java
@@ -5,6 +5,27 @@ public final class MariaDBBugs {
     // https://jira.mariadb.org/browse/MDEV-21058
     public static boolean bug21058 = true;
 
+    // https://jira.mariadb.org/browse/MDEV-32076
+    public static boolean bug32076 = true;
+
+    // https://jira.mariadb.org/browse/MDEV-32099
+    public static boolean bug32099 = true;
+
+    // https://jira.mariadb.org/browse/MDEV-32105
+    public static boolean bug32105 = true;
+
+    // https://jira.mariadb.org/browse/MDEV-32106
+    public static boolean bug32106 = true;
+
+    // https://jira.mariadb.org/browse/MDEV-32107
+    public static boolean bug32107 = true;
+
+    // https://jira.mariadb.org/browse/MDEV-32108
+    public static boolean bug32108 = true;
+
+    // https://jira.mariadb.org/browse/MDEV-32143
+    public static boolean bug32143 = true;
+
     private MariaDBBugs() {
     }
 
--- src/sqlancer/mariadb/MariaDBErrors.java
+++ src/sqlancer/mariadb/MariaDBErrors.java
@@ -7,6 +7,23 @@ public final class MariaDBErrors {
     private MariaDBErrors() {
     }
 
+    public static void addCommonErrors(ExpectedErrors errors) {
+        errors.add("is out of range");
+        // regex
+        errors.add("unmatched parentheses");
+        errors.add("nothing to repeat at offset");
+        errors.add("missing )");
+        errors.add("missing terminating ]");
+        errors.add("range out of order in character class");
+        errors.add("unrecognized character after ");
+        errors.add("Got error '(*VERB) not recognized or malformed");
+        errors.add("must be followed by");
+        errors.add("malformed number or name after");
+        errors.add("digit expected after");
+        errors.add("Regex error");
+        errors.add("Lock wait timeout exceeded");
+    }
+
     public static void addInsertErrors(ExpectedErrors errors) {
         errors.add("Out of range");
         errors.add("Duplicate entry"); // violates UNIQUE constraint
