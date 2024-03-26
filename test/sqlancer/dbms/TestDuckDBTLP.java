b68bc6e6f788b21b3b383fecb0c7fba9bf24f10d
malwaregarry <malwaregarry@gmail.com>
2024-03-26 17:42:07 +0800

    Include NoREC in DuckDB unit tests

--- test/sqlancer/dbms/TestDuckDBTLP.java
+++ test/sqlancer/dbms/TestDuckDBTLP.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 
 import sqlancer.Main;
 
-public class TestDuckDB {
+public class TestDuckDBTLP {
 
     @Test
-    public void testDuckDB() {
+    public void testDuckDBTLP() {
         // run with one thread due to multithreading issues, see https://github.com/sqlancer/sqlancer/pull/45
         assertEquals(0,
                 Main.executeMain(new String[] { "--random-seed", "0", "--timeout-seconds", TestConfig.SECONDS,
