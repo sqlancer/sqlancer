031dbeb57d4c494f2ac2b09d9551c2473714a758
malwaregarry <malwaregarry@gmail.com>
2024-04-11 14:43:33 +0800

    Use generic TLP where oracle for Postgres

--- src/sqlancer/postgres/oracle/tlp/PostgresTLPWhereOracle.java
+++ src/sqlancer/postgres/oracle/tlp/PostgresTLPWhereOracle.java
@@ -1,45 +1,40 @@
 package sqlancer.postgres.oracle.tlp;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
-import sqlancer.ComparatorHelper;
-import sqlancer.Randomly;
+import sqlancer.common.oracle.TLPWhereOracle;
+import sqlancer.common.oracle.TestOracle;
+import sqlancer.common.query.ExpectedErrors;
 import sqlancer.postgres.PostgresGlobalState;
-import sqlancer.postgres.PostgresVisitor;
+import sqlancer.postgres.PostgresSchema;
+import sqlancer.postgres.PostgresSchema.PostgresColumn;
+import sqlancer.postgres.PostgresSchema.PostgresTable;
+import sqlancer.postgres.ast.PostgresExpression;
+import sqlancer.postgres.ast.PostgresJoin;
+import sqlancer.postgres.ast.PostgresSelect;
+import sqlancer.postgres.gen.PostgresCommon;
+import sqlancer.postgres.gen.PostgresExpressionGenerator;
 
-public class PostgresTLPWhereOracle extends PostgresTLPBase {
+public class PostgresTLPWhereOracle implements TestOracle<PostgresGlobalState> {
+
+    private final TLPWhereOracle<PostgresSelect, PostgresJoin, PostgresExpression, PostgresSchema, PostgresTable, PostgresColumn, PostgresGlobalState> oracle;
 
     public PostgresTLPWhereOracle(PostgresGlobalState state) {
-        super(state);
+        PostgresExpressionGenerator gen = new PostgresExpressionGenerator(state);
+        ExpectedErrors expectedErrors = ExpectedErrors.newErrors().with(PostgresCommon.getCommonExpressionErrors())
+                .with(PostgresCommon.getCommonFetchErrors()).withRegex(PostgresCommon.getCommonExpressionRegexErrors())
+                .build();
+
+        this.oracle = new TLPWhereOracle<>(state, gen, expectedErrors);
     }
 
     @Override
     public void check() throws SQLException {
-        super.check();
-        whereCheck();
+        oracle.check();
     }
 
-    protected void whereCheck() throws SQLException {
-        if (Randomly.getBooleanWithRatherLowProbability()) {
-            select.setOrderByClauses(gen.generateOrderBy());
-        }
-        String originalQueryString = PostgresVisitor.asString(select);
-        List<String> resultSet = ComparatorHelper.getResultSetFirstColumnAsString(originalQueryString, errors, state);
-
-        select.setOrderByClauses(Collections.emptyList());
-        select.setWhereClause(predicate);
-        String firstQueryString = PostgresVisitor.asString(select);
-        select.setWhereClause(negatedPredicate);
-        String secondQueryString = PostgresVisitor.asString(select);
-        select.setWhereClause(isNullPredicate);
-        String thirdQueryString = PostgresVisitor.asString(select);
-        List<String> combinedString = new ArrayList<>();
-        List<String> secondResultSet = ComparatorHelper.getCombinedResultSet(firstQueryString, secondQueryString,
-                thirdQueryString, combinedString, Randomly.getBoolean(), state, errors);
-        ComparatorHelper.assumeResultSetsAreEqual(resultSet, secondResultSet, originalQueryString, combinedString,
-                state);
+    @Override
+    public String getLastQueryString() {
+        return oracle.getLastQueryString();
     }
 }
