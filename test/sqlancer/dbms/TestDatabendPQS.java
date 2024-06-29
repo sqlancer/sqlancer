3989d762ede46b8251a104fa66a294aa27897271
malwaregarry <malwaregarry@gmail.com>
2024-06-29 16:27:43 +0800

    [Databend] Refactor tests

---
+++ test/sqlancer/dbms/TestDatabendPQS.java
@@ -0,0 +1,23 @@
+package sqlancer.dbms;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import org.junit.jupiter.api.Test;
+
+import sqlancer.Main;
+import sqlancer.Randomly;
+
+public class TestDatabendPQS {
+
+    @Test
+    public void testDatabendPQS() {
+        assumeTrue(TestConfig.isEnvironmentTrue(TestConfig.DATABEND_ENV));
+        assertEquals(0,
+                Main.executeMain("--random-seed", "0", "--timeout-seconds", TestConfig.SECONDS, "--num-threads", "4",
+                        "--num-queries", TestConfig.NUM_QUERIES, "--database-prefix", "databend",
+                        "--random-string-generation", String.valueOf(Randomly.StringGenerationStrategy.ALPHANUMERIC),
+                        "--host", "127.0.0.1", "--port", "3307", "databend", "--oracle", "PQS"));
+    }
+
+}
