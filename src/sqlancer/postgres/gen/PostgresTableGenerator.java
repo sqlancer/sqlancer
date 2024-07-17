0316c1c9c2fa2ad2a82977cfe2587faaba78cec8
Robins Tharakan <tharakan@gmail.com>
2024-07-17 12:36:27 +0930

    Ensure INHERITS() only uses tables (not views)
    In Postgres, CREATE TABLE INHERITS () throws an error when VIEWs are
    provided, sample given below.
    
    ERROR: inherited relation "pg_buffercache" is not a table or foreign table
    
    Ref:
    1. https://www.postgresql.org/docs/current/ddl-inherit.html

--- src/sqlancer/postgres/gen/PostgresTableGenerator.java
+++ src/sqlancer/postgres/gen/PostgresTableGenerator.java
@@ -207,7 +207,7 @@ public class PostgresTableGenerator {
     }
 
     private void generateInherits() {
-        if (Randomly.getBoolean() && !newSchema.getDatabaseTables().isEmpty()) {
+        if (Randomly.getBoolean() && !newSchema.getDatabaseTablesWithoutViews().isEmpty()) {
             sb.append(" INHERITS(");
             sb.append(newSchema.getDatabaseTablesRandomSubsetNotEmpty().stream().map(t -> t.getName())
                     .collect(Collectors.joining(", ")));
