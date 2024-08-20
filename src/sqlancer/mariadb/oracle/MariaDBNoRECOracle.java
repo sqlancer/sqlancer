6e10142ca30d7134edb38685fcd1ed0537599382
malwaregarry <malwaregarry@gmail.com>
2024-08-20 13:35:10 +0800

    [MariaDB] Use generic NoREC oracle

--- src/sqlancer/mariadb/oracle/MariaDBNoRECOracle.java
+++ src/sqlancer/mariadb/oracle/MariaDBNoRECOracle.java
@@ -1,126 +1,46 @@
 package sqlancer.mariadb.oracle;
 
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
-import sqlancer.IgnoreMeException;
-import sqlancer.common.oracle.NoRECBase;
+import sqlancer.Reproducer;
+import sqlancer.common.oracle.NoRECOracle;
 import sqlancer.common.oracle.TestOracle;
-import sqlancer.common.query.SQLQueryAdapter;
-import sqlancer.common.query.SQLancerResultSet;
+import sqlancer.common.query.ExpectedErrors;
 import sqlancer.mariadb.MariaDBProvider.MariaDBGlobalState;
 import sqlancer.mariadb.MariaDBSchema;
 import sqlancer.mariadb.MariaDBSchema.MariaDBColumn;
-import sqlancer.mariadb.MariaDBSchema.MariaDBDataType;
 import sqlancer.mariadb.MariaDBSchema.MariaDBTable;
-import sqlancer.mariadb.ast.MariaDBAggregate;
-import sqlancer.mariadb.ast.MariaDBAggregate.MariaDBAggregateFunction;
-import sqlancer.mariadb.ast.MariaDBColumnName;
 import sqlancer.mariadb.ast.MariaDBExpression;
-import sqlancer.mariadb.ast.MariaDBPostfixUnaryOperation;
-import sqlancer.mariadb.ast.MariaDBPostfixUnaryOperation.MariaDBPostfixUnaryOperator;
+import sqlancer.mariadb.ast.MariaDBJoin;
 import sqlancer.mariadb.ast.MariaDBSelectStatement;
-import sqlancer.mariadb.ast.MariaDBSelectStatement.MariaDBSelectType;
-import sqlancer.mariadb.ast.MariaDBTableReference;
-import sqlancer.mariadb.ast.MariaDBText;
-import sqlancer.mariadb.ast.MariaDBVisitor;
 import sqlancer.mariadb.gen.MariaDBExpressionGenerator;
 
-public class MariaDBNoRECOracle extends NoRECBase<MariaDBGlobalState> implements TestOracle<MariaDBGlobalState> {
+public class MariaDBNoRECOracle implements TestOracle<MariaDBGlobalState> {
 
-    private final MariaDBSchema s;
-    private static final int NOT_FOUND = -1;
+    NoRECOracle<MariaDBSelectStatement, MariaDBJoin, MariaDBExpression, MariaDBSchema, MariaDBTable, MariaDBColumn, MariaDBGlobalState> oracle;
 
     public MariaDBNoRECOracle(MariaDBGlobalState globalState) {
-        super(globalState);
-        this.s = globalState.getSchema();
-        errors.add("is out of range");
-        // regex
-        errors.add("unmatched parentheses");
-        errors.add("nothing to repeat at offset");
-        errors.add("missing )");
-        errors.add("missing terminating ]");
-        errors.add("range out of order in character class");
-        errors.add("unrecognized character after ");
-        errors.add("Got error '(*VERB) not recognized or malformed");
-        errors.add("must be followed by");
-        errors.add("malformed number or name after");
-        errors.add("digit expected after");
+        MariaDBExpressionGenerator gen = new MariaDBExpressionGenerator(globalState.getRandomly());
+        ExpectedErrors errors = ExpectedErrors.newErrors().with("is out of range").with("unmatched parentheses")
+                .with("nothing to repeat at offset").with("missing )").with("missing terminating ]")
+                .with("range out of order in character class").with("unrecognized character after ")
+                .with("Got error '(*VERB) not recognized or malformed").with("must be followed by")
+                .with("malformed number or name after").with("digit expected after").build();
+        this.oracle = new NoRECOracle<>(globalState, gen, errors);
     }
 
     @Override
     public void check() throws SQLException {
-        MariaDBTable randomTable = s.getRandomTable();
-        List<MariaDBColumn> columns = randomTable.getColumns();
-        MariaDBExpressionGenerator gen = new MariaDBExpressionGenerator(state.getRandomly()).setColumns(columns)
-                .setCon(con).setState(state.getState());
-        MariaDBExpression randomWhereCondition = gen.getRandomExpression();
-        List<MariaDBExpression> groupBys = Collections.emptyList(); // getRandomExpressions(columns);
-        int optimizedCount = getOptimizedQuery(randomTable, randomWhereCondition, groupBys);
-        int unoptimizedCount = getUnoptimizedQuery(randomTable, randomWhereCondition, groupBys);
-        if (optimizedCount == NOT_FOUND || unoptimizedCount == NOT_FOUND) {
-            throw new IgnoreMeException();
-        }
-        if (optimizedCount != unoptimizedCount) {
-            state.getState().getLocalState().log(optimizedQueryString + ";\n" + unoptimizedQueryString + ";");
-            throw new AssertionError(optimizedCount + " " + unoptimizedCount);
-        }
+        oracle.check();
     }
 
-    private int getUnoptimizedQuery(MariaDBTable randomTable, MariaDBExpression randomWhereCondition,
-            List<MariaDBExpression> groupBys) throws SQLException {
-        MariaDBSelectStatement select = new MariaDBSelectStatement();
-        select.setGroupByClause(groupBys);
-        MariaDBPostfixUnaryOperation isTrue = new MariaDBPostfixUnaryOperation(MariaDBPostfixUnaryOperator.IS_TRUE,
-                randomWhereCondition);
-        MariaDBText asText = new MariaDBText(isTrue, " as count", false);
-        select.setFetchColumns(Arrays.asList(asText));
-        select.setFromList(Arrays.asList(new MariaDBTableReference(randomTable)));
-        select.setSelectType(MariaDBSelectType.ALL);
-        int secondCount = 0;
-
-        unoptimizedQueryString = "SELECT SUM(count) FROM (" + MariaDBVisitor.asString(select) + ") as asdf";
-        SQLQueryAdapter q = new SQLQueryAdapter(unoptimizedQueryString, errors);
-        try (SQLancerResultSet rs = q.executeAndGet(state)) {
-            if (rs == null) {
-                return NOT_FOUND;
-            } else {
-                while (rs.next()) {
-                    secondCount = rs.getInt(1);
-                }
-            }
-        }
-
-        return secondCount;
+    @Override
+    public Reproducer<MariaDBGlobalState> getLastReproducer() {
+        return oracle.getLastReproducer();
     }
 
-    private int getOptimizedQuery(MariaDBTable randomTable, MariaDBExpression randomWhereCondition,
-            List<MariaDBExpression> groupBys) throws SQLException {
-        MariaDBSelectStatement select = new MariaDBSelectStatement();
-        select.setGroupByClause(groupBys);
-        MariaDBAggregate aggr = new MariaDBAggregate(
-                new MariaDBColumnName(new MariaDBColumn("*", MariaDBDataType.INT, false, 0)),
-                MariaDBAggregateFunction.COUNT);
-        select.setFetchColumns(Arrays.asList(aggr));
-        select.setFromList(Arrays.asList(new MariaDBTableReference(randomTable)));
-        select.setWhereClause(randomWhereCondition);
-        select.setSelectType(MariaDBSelectType.ALL);
-        int firstCount;
-        optimizedQueryString = MariaDBVisitor.asString(select);
-        SQLQueryAdapter q = new SQLQueryAdapter(optimizedQueryString, errors);
-        try (SQLancerResultSet rs = q.executeAndGet(state)) {
-            if (rs == null) {
-                firstCount = NOT_FOUND;
-            } else {
-                rs.next();
-                firstCount = rs.getInt(1);
-            }
-        } catch (Exception e) {
-            throw new AssertionError(optimizedQueryString, e);
-        }
-        return firstCount;
+    @Override
+    public String getLastQueryString() {
+        return oracle.getLastQueryString();
     }
-
 }
