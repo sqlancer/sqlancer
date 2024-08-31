993dd64c7676d1bdc28356fdfffd87ce178294ee
Manuel Rigger <rigger@nus.edu.sg>
2024-08-31 23:22:20 +0800

    Add an expected error for mismatched expression types in the coalesce operator
    SELECT COALESCE(null, 'a', 5); -- Binder Error: Cannot mix values of type VARCHAR and INTEGER_LITERAL in COALESCE operator - an explicit cast is required (exit status 1)

--- src/sqlancer/duckdb/DuckDBErrors.java
+++ src/sqlancer/duckdb/DuckDBErrors.java
@@ -62,6 +62,7 @@ public final class DuckDBErrors {
         errors.addAll(getExpressionErrors());
         errors.addRegexString("Binder Error: Cannot mix values of type .* and .* in BETWEEN clause");
         errors.addRegexString("Binder Error: Cannot mix values of type .* and .* in CASE expression");
+        errors.addRegexString("Cannot mix values of type .* and .* in COALESCE operator");
         errors.addRegexString("Cannot compare values of type .* and type .*");
     }
 
