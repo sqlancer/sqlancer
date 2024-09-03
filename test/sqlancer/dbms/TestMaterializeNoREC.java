7fddc4bce9c513dc5b47f1f44f2f34e7bec5f056
malwaregarry <malwaregarry@gmail.com>
2024-09-03 13:49:41 +0800

    [Materialize] Update tests

--- test/sqlancer/dbms/TestMaterializeNoREC.java
+++ test/sqlancer/dbms/TestMaterializeNoREC.java
@@ -7,16 +7,15 @@ import org.junit.jupiter.api.Test;
 
 import sqlancer.Main;
 
-public class TestMaterializePQS {
+public class TestMaterializeNoREC {
 
     @Test
     public void test() {
         assumeTrue(TestConfig.isEnvironmentTrue(TestConfig.MATERIALIZE_ENV));
         assertEquals(0,
                 Main.executeMain(new String[] { "--random-seed", "0", "--timeout-seconds", TestConfig.SECONDS,
-                        "--num-threads", "4", "--num-queries", TestConfig.NUM_QUERIES, "--random-string-generation",
-                        "ALPHANUMERIC_SPECIALCHAR", "--username", "materialize", "materialize", "--oracle", "pqs",
-                        "--set-max-tables-mvs", "true" }));
+                        "--num-threads", "4", "--num-queries", TestConfig.NUM_QUERIES, "--username", "materialize",
+                        "materialize", "--oracle", "NOREC", "--set-max-tables-mvs", "true" }));
     }
 
 }
