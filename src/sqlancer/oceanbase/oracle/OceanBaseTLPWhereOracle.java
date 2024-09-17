6709c8a8b30507630a4772c053a484ced79f865d
malwaregarry <malwaregarry@gmail.com>
2024-09-17 11:09:37 +0800

    [OceanBase] Use common TLP-where oracle

--- src/sqlancer/oceanbase/oracle/OceanBaseTLPWhereOracle.java
+++ src/sqlancer/oceanbase/oracle/OceanBaseTLPWhereOracle.java
@@ -1,44 +1,45 @@
 package sqlancer.oceanbase.oracle;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
-import sqlancer.ComparatorHelper;
-import sqlancer.Randomly;
+import sqlancer.Reproducer;
+import sqlancer.common.oracle.TLPWhereOracle;
+import sqlancer.common.oracle.TestOracle;
+import sqlancer.common.query.ExpectedErrors;
+import sqlancer.oceanbase.OceanBaseErrors;
 import sqlancer.oceanbase.OceanBaseGlobalState;
-import sqlancer.oceanbase.OceanBaseVisitor;
+import sqlancer.oceanbase.OceanBaseSchema;
+import sqlancer.oceanbase.OceanBaseSchema.OceanBaseColumn;
+import sqlancer.oceanbase.OceanBaseSchema.OceanBaseTable;
+import sqlancer.oceanbase.ast.OceanBaseExpression;
+import sqlancer.oceanbase.ast.OceanBaseJoin;
+import sqlancer.oceanbase.ast.OceanBaseSelect;
+import sqlancer.oceanbase.gen.OceanBaseExpressionGenerator;
 
-public class OceanBaseTLPWhereOracle extends OceanBaseTLPBase {
+public class OceanBaseTLPWhereOracle implements TestOracle<OceanBaseGlobalState> {
+
+    private final TLPWhereOracle<OceanBaseSelect, OceanBaseJoin, OceanBaseExpression, OceanBaseSchema, OceanBaseTable, OceanBaseColumn, OceanBaseGlobalState> oracle;
 
     public OceanBaseTLPWhereOracle(OceanBaseGlobalState state) {
-        super(state);
+        OceanBaseExpressionGenerator gen = new OceanBaseExpressionGenerator(state);
+        ExpectedErrors expectedErrors = ExpectedErrors.newErrors().with(OceanBaseErrors.getExpressionErrors())
+                .withRegex(OceanBaseErrors.getExpressionErrorsRegex()).with("value is out of range").build();
+
+        this.oracle = new TLPWhereOracle<>(state, gen, expectedErrors);
     }
 
     @Override
     public void check() throws SQLException {
-        super.check();
-        select.setWhereClause(null);
-        String originalQueryString = OceanBaseVisitor.asString(select);
-
-        List<String> resultSet = ComparatorHelper.getResultSetFirstColumnAsString(originalQueryString, errors, state);
-
-        if (Randomly.getBoolean()) {
-            select.setOrderByClauses(gen.generateOrderBys());
-        }
-        select.setOrderByClauses(Collections.emptyList());
-        select.setWhereClause(predicate);
-        String firstQueryString = OceanBaseVisitor.asString(select);
-        select.setWhereClause(negatedPredicate);
-        String secondQueryString = OceanBaseVisitor.asString(select);
-        select.setWhereClause(isNullPredicate);
-        String thirdQueryString = OceanBaseVisitor.asString(select);
-        List<String> combinedString = new ArrayList<>();
-        List<String> secondResultSet = ComparatorHelper.getCombinedResultSet(firstQueryString, secondQueryString,
-                thirdQueryString, combinedString, Randomly.getBoolean(), state, errors);
-        ComparatorHelper.assumeResultSetsAreEqual(resultSet, secondResultSet, originalQueryString, combinedString,
-                state);
+        oracle.check();
     }
 
+    @Override
+    public String getLastQueryString() {
+        return oracle.getLastQueryString();
+    }
+
+    @Override
+    public Reproducer<OceanBaseGlobalState> getLastReproducer() {
+        return oracle.getLastReproducer();
+    }
 }
