72f73677471707fc632793a5585d1abcbced6a69
Manuel Rigger <rigger@nus.edu.sg>
2025-01-10 17:03:55 +0800

    Remove workaround code for a fixed DuckDB bug

--- src/sqlancer/duckdb/DuckDBBugs.java
+++ src/sqlancer/duckdb/DuckDBBugs.java
@@ -2,9 +2,6 @@ package sqlancer.duckdb;
 
 public final class DuckDBBugs {
 
-    // https://github.com/duckdb/duckdb/issues/13933
-    public static boolean bug13933 = true;
-
     private DuckDBBugs() {
     }
 
