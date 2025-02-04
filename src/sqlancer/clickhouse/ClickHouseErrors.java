3c08f72bb9d796dbcdb6a04a352f008ef9d44623
Ilya Yatsishin <2159081+qoega@users.noreply.github.com>
2025-02-04 18:00:58 +0100

    Update ClickHouseErrors.java

--- src/sqlancer/clickhouse/ClickHouseErrors.java
+++ src/sqlancer/clickhouse/ClickHouseErrors.java
@@ -55,6 +55,7 @@ public final class ClickHouseErrors {
                 // AST generation logic
                 "MULTIPLE_EXPRESSIONS_FOR_ALIAS", "AMBIGUOUS_IDENTIFIER", // https://github.com/ClickHouse/ClickHouse/issues/45389
                 "AMBIGUOUS_COLUMN_NAME", // same https://github.com/ClickHouse/ClickHouse/issues/45389
+                "No equality condition found in JOIN ON expression",
                 "Cannot parse number with multiple sign");
     }
 
