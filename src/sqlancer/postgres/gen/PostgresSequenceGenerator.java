08721eaefdb7448018e48c02eeef734d8ef195a1
liauzhanyi <liauzhanyi@gmail.com>
2025-02-17 21:55:44 +0800

    Refactor: add createSequence method to PostgresSequenceGenerator and YSQLSequenceGenerator

--- src/sqlancer/postgres/gen/PostgresSequenceGenerator.java
+++ src/sqlancer/postgres/gen/PostgresSequenceGenerator.java
@@ -1,11 +1,16 @@
 package sqlancer.postgres.gen;
 
 import sqlancer.common.gen.AbstractSequenceGenerator;
+import sqlancer.common.query.SQLQueryAdapter;
 import sqlancer.postgres.PostgresGlobalState;
 
 public final class PostgresSequenceGenerator extends AbstractSequenceGenerator<PostgresGlobalState> {
 
     private PostgresSequenceGenerator() {
+    }
+
+    public static SQLQueryAdapter createSequence(PostgresGlobalState globalState) {
+        return AbstractSequenceGenerator.createSequence(globalState);
     }
 
 }
--- src/sqlancer/yugabyte/ysql/gen/YSQLSequenceGenerator.java
+++ src/sqlancer/yugabyte/ysql/gen/YSQLSequenceGenerator.java
@@ -1,6 +1,7 @@
 package sqlancer.yugabyte.ysql.gen;
 
 import sqlancer.common.gen.AbstractSequenceGenerator;
+import sqlancer.common.query.SQLQueryAdapter;
 import sqlancer.yugabyte.ysql.YSQLGlobalState;
 
 public final class YSQLSequenceGenerator extends AbstractSequenceGenerator<YSQLGlobalState> {
@@ -8,4 +9,7 @@ public final class YSQLSequenceGenerator extends AbstractSequenceGenerator<YSQLG
     private YSQLSequenceGenerator() {
     }
 
+    public static SQLQueryAdapter createSequence(YSQLGlobalState globalState) {
+        return AbstractSequenceGenerator.createSequence(globalState);
+    }
 }
