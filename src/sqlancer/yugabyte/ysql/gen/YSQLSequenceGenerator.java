bbfa924919d0ea0dcf880c2b7206464a02ae0152
liauzhanyi <liauzhanyi@gmail.com>
2025-02-19 21:37:21 +0800

    Revert "Refactor: add createSequence method to PostgresSequenceGenerator and YSQLSequenceGenerator"
    This reverts commit 08721eaefdb7448018e48c02eeef734d8ef195a1.

--- src/sqlancer/yugabyte/ysql/gen/YSQLSequenceGenerator.java
+++ src/sqlancer/yugabyte/ysql/gen/YSQLSequenceGenerator.java
@@ -1,7 +1,6 @@
 package sqlancer.yugabyte.ysql.gen;
 
 import sqlancer.common.gen.AbstractSequenceGenerator;
-import sqlancer.common.query.SQLQueryAdapter;
 import sqlancer.yugabyte.ysql.YSQLGlobalState;
 
 public final class YSQLSequenceGenerator extends AbstractSequenceGenerator<YSQLGlobalState> {
@@ -9,7 +8,4 @@ public final class YSQLSequenceGenerator extends AbstractSequenceGenerator<YSQLG
     private YSQLSequenceGenerator() {
     }
 
-    public static SQLQueryAdapter createSequence(YSQLGlobalState globalState) {
-        return AbstractSequenceGenerator.createSequence(globalState);
-    }
 }
