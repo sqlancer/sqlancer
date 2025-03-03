ecfa44497eb672e6be1e9ac76067967180579e7d
abdullah <elgendyabdala@gmail.com>
2025-03-03 15:55:40 +0200

    Refactor: Extract generateIndexColumns() helper

--- src/sqlancer/mysql/MySQLSchema.java
+++ src/sqlancer/mysql/MySQLSchema.java
@@ -27,7 +27,7 @@ public class MySQLSchema extends AbstractSchema<MySQLGlobalState, MySQLTable> {
     private static final int NR_SCHEMA_READ_TRIES = 10;
 
     public enum MySQLDataType {
-        INT, VARCHAR, FLOAT, DOUBLE, DECIMAL , TEXT, LONGTEXT;
+        INT, VARCHAR, FLOAT, DOUBLE, DECIMAL;
 
         public static MySQLDataType getRandom(MySQLGlobalState globalState) {
             if (globalState.usesPQS()) {
