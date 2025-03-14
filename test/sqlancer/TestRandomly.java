1667b6c1d9d7917e01e4b0f013d452f43c0d66c0
abdullah <elgendyabdala@gmail.com>
2025-03-14 13:44:17 +0200

    Apply code formatting

--- test/sqlancer/TestRandomly.java
+++ test/sqlancer/TestRandomly.java
@@ -1,5 +1,6 @@
 package sqlancer;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
