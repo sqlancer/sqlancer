52cd26ae18c08e4b6021f5c6c5a8971eb760b4db
yewjon <e0726230@u.nus.edu>
2025-03-18 01:32:21 +0800

    Fix format error

--- src/sqlancer/cockroachdb/CockroachDBUtils.java
+++ src/sqlancer/cockroachdb/CockroachDBUtils.java
@@ -5,7 +5,6 @@ import sqlancer.common.ast.JoinBase.JoinType;
 
 public final class CockroachDBUtils {
 
-
     private CockroachDBUtils() {
     }
 
@@ -17,9 +16,11 @@ public final class CockroachDBUtils {
             selectedJoinType = CockroachDBJoin.JoinType.getRandomExcept("COCKROACHDB", JoinType.NATURAL, JoinType.CROSS,
                     JoinType.LEFT, JoinType.RIGHT);
         } else if (join.getJoinType() == JoinType.FULL) {
-            selectedJoinType = CockroachDBJoin.JoinType.getRandomExcept("COCKROACHDB", JoinType.NATURAL, JoinType.CROSS);
+            selectedJoinType = CockroachDBJoin.JoinType.getRandomExcept("COCKROACHDB", JoinType.NATURAL,
+                    JoinType.CROSS);
         } else if (join.getJoinType() != JoinType.CROSS) {
-            selectedJoinType = CockroachDBJoin.JoinType.getRandomExcept("COCKROACHDB", JoinType.NATURAL, join.getJoinType());
+            selectedJoinType = CockroachDBJoin.JoinType.getRandomExcept("COCKROACHDB", JoinType.NATURAL,
+                    join.getJoinType());
         }
 
         assert selectedJoinType != JoinType.NATURAL; // Natural Join is not supported for CERT
