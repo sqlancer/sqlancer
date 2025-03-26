7c009aff86d6cac34d3f54e70a505d587610b328
abdullah <elgendyabdala@gmail.com>
2025-03-26 14:14:42 +0200

    Add more aggregate functions (AVG, GROUP_CONCAT)

--- src/sqlancer/mysql/ast/MySQLAggregate.java
+++ src/sqlancer/mysql/ast/MySQLAggregate.java
@@ -1,6 +1,7 @@
 package sqlancer.mysql.ast;
 
 import java.util.List;
+import sqlancer.Randomly;
 
 public class MySQLAggregate implements MySQLExpression {
 
@@ -12,7 +13,11 @@ public class MySQLAggregate implements MySQLExpression {
         // See https://dev.mysql.com/doc/refman/8.4/en/aggregate-functions.html#function_min.
         MIN("MIN", null, false), MIN_DISTINCT("MIN", "DISTINCT", false),
         // See https://dev.mysql.com/doc/refman/8.4/en/aggregate-functions.html#function_max.
-        MAX("MAX", null, false), MAX_DISTINCT("MAX", "DISTINCT", false);
+        MAX("MAX", null, false), MAX_DISTINCT("MAX", "DISTINCT", false),
+        // See https://dev.mysql.com/doc/refman/8.4/en/aggregate-functions.html#function_avg.
+        AVG("AVG", null, false), AVG_DISTINCT("AVG", "DISTINCT", false),
+        // See https://dev.mysql.com/doc/refman/8.4/en/aggregate-functions.html#function_group-concat.
+        GROUP_CONCAT("GROUP_CONCAT", null, true), GROUP_CONCAT_DISTINCT("GROUP_CONCAT", "DISTINCT", true);
 
         private final String name;
         private final String option;
@@ -35,10 +40,15 @@ public class MySQLAggregate implements MySQLExpression {
         public boolean isVariadic() {
             return this.isVariadic;
         }
+
+        public static MySQLAggregateFunction getRandom() {
+            return Randomly.fromOptions(values());
+        }
     }
 
     private final List<MySQLExpression> exprs;
     private final MySQLAggregateFunction func;
+    private String windowSpecification;
 
     public MySQLAggregate(List<MySQLExpression> exprs, MySQLAggregateFunction func) {
         this.exprs = exprs;
@@ -51,5 +61,14 @@ public class MySQLAggregate implements MySQLExpression {
 
     public MySQLAggregateFunction getFunc() {
         return func;
+    }
+
+    public void setWindowSpecification(String windowSpec) {
+        this.windowSpecification = windowSpec;
+    }
+
+
+    public boolean isWindowFunction() {
+        return windowSpecification != null;
     }
 }
