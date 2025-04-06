f2b33c90a26b835622d520757b4120ef17e94df0
Pratyksh Gupta <pratykshgupta9999@gmail.com>
2025-04-06 14:32:38 +0530

    Update PostgresGlobalState.java

--- src/sqlancer/postgres/PostgresGlobalState.java
+++ src/sqlancer/postgres/PostgresGlobalState.java
@@ -27,7 +27,6 @@ public class PostgresGlobalState extends SQLGlobalState<PostgresOptions, Postgre
     // store and allow filtering by function volatility classifications
     private final Map<String, Character> functionsAndTypes = new HashMap<>();
     private List<Character> allowedFunctionTypes = Arrays.asList(IMMUTABLE, STABLE, VOLATILE);
-    private int majorVersion;
 
     @Override
     public void setConnection(SQLConnection con) {
@@ -37,7 +36,6 @@ public class PostgresGlobalState extends SQLGlobalState<PostgresOptions, Postgre
             this.operators = getOperators(getConnection());
             this.collates = getCollnames(getConnection());
             this.tableAccessMethods = getTableAccessMethods(getConnection());
-            this.majorVersion = getMajorVersion(getConnection());
         } catch (SQLException e) {
             throw new AssertionError(e);
         }
@@ -95,19 +93,6 @@ public class PostgresGlobalState extends SQLGlobalState<PostgresOptions, Postgre
         return tableAccessMethods;
     }
 
-    private int getMajorVersion(SQLConnection con) throws SQLException {
-        try (Statement s = con.createStatement()) {
-            try (ResultSet rs = s.executeQuery("SHOW server_version_num;")) {
-                if (rs.next()) {
-                    // Server version returns a number like 130018 for 13.18
-                    // Divide by 10000 to get major version (13)
-                    return rs.getInt(1) / 10000;
-                }
-            }
-        }
-        return 0; // Default to 0 if version cannot be determined
-    }
-
     public List<String> getOperators() {
         return operators;
     }
@@ -163,10 +148,6 @@ public class PostgresGlobalState extends SQLGlobalState<PostgresOptions, Postgre
 
     public List<Character> getAllowedFunctionTypes() {
         return this.allowedFunctionTypes;
-    }
-
-    public int getMajorVersion() {
-        return majorVersion;
     }
 
 }
