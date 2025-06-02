56368ebea110bac06511d58522ba49954fa78148
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2025-06-02 09:25:58 -0700

    Added Restarting a DDL transaction not supported for database creation code

--- src/sqlancer/yugabyte/ysql/YSQLProvider.java
+++ src/sqlancer/yugabyte/ysql/YSQLProvider.java
@@ -194,7 +194,7 @@ public class YSQLProvider extends SQLProviderAdapter<YSQLGlobalState, YSQLOption
             } catch (Exception e) {
                 if ((e.getMessage().contains("Catalog Version Mismatch") || e.getMessage().contains("Restart read required")
                         || e.getMessage().contains("could not serialize access due to concurrent update"))
-                        || e.getMessage().contains("is being accessed by other users")
+                        || e.getMessage().contains("is being accessed by other users") || e.getMessage().contains("Restarting a DDL transaction not supported")
                         && counter < 20) {
                     counter++;
                     exceptionLessSleep(500);
