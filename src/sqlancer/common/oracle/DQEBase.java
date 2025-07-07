d96faee27942fb7f594235cb16e3a02a035ac0b3
HDQ <hluli182@gmail.com>
2025-07-07 17:20:26 +0800

    add doc for DQEBase.java

--- src/sqlancer/common/oracle/DQEBase.java
+++ src/sqlancer/common/oracle/DQEBase.java
@@ -15,6 +15,24 @@ import sqlancer.common.query.SQLQueryAdapter;
 import sqlancer.common.query.SQLQueryError;
 import sqlancer.common.schema.AbstractRelationalTable;
 
+/*
+ * In DBMSs, SELECT, UPDATE and DELETE queries utilize predicates (i.e., WHERE clauses) to specify which rows to retrieve, update or delete, respectively.
+ * If they use the same predicate φ, they should access the same rows in a database.
+ * Ideally, DBMSs can adopt the same implementations for predicate evaluation in SELECT, UPDATE and DELETE queries.
+ * However, a DBMS usually adopts different implementations for predicate evaluation in SELECT, UPDATE and DELETE queries due to various optimization choices.
+ * Inconsistent implementations for predicate evaluation among these queries can cause SELECT, UPDATE and DELETE queries with the same predicate φ to access different rows.
+ *
+ *
+ * Inspired by this key observation, we propose Differential Query Execution(DQE), a novel and general approach to detect logic bugs in SELECT, UPDATE and DELETE queries.
+ * DQE solves the test oracle problem by executing SELECT, UPDATE and DELETE queries with the same predicate φ, and observing inconsistencies among their execution results.
+ * For example, if a row that is updated by an UPDATE query with a predicate φ does not appear in the query result of a SELECT query with the same predicate φ, a logic bug is detected in the target DBMS.
+ * The key challenge of DQE is to automatically obtain the accessed rows for a given SELECT, UPDATE or DELETE query.
+ * To address this challenge, we append two extra columns to each table in a database, to uniquely identify each row and track whether a row has been modified, respectively.
+ * We further rewrite SELECT and UPDATE queries to identify their accessed rows.
+ *
+ * more information see [DQE paper](https://ieeexplore.ieee.org/document/10172736)
+ */
+
 public abstract class DQEBase<S extends SQLGlobalState<?, ?>> {
 
     public static String COLUMN_ROWID = "rowId";
@@ -36,6 +54,13 @@ public abstract class DQEBase<S extends SQLGlobalState<?, ?>> {
         this.options = state.getOptions();
     }
 
+    /**
+     * Add auxiliary columns to the database
+     * A abstract method, subclasses need to implement it
+     *
+     * @param table
+     * @throws SQLException
+     */
     public abstract void addAuxiliaryColumns(AbstractRelationalTable<?, ?, ?> table) throws SQLException;
 
     public void dropAuxiliaryColumns(AbstractRelationalTable<?, ?, ?> table) throws SQLException {
@@ -46,6 +71,10 @@ public abstract class DQEBase<S extends SQLGlobalState<?, ?>> {
         new SQLQueryAdapter(dropColumnUpdated).execute(state);
     }
 
+    /**
+     * The core idea of DQE is that the SELECT, UPDATE and DELETE queries with the same predicate φ should access the same rows.
+     * If these queries access different rows, DQE reveals a potential logic bug in the target DBMS.
+     */
     public static class SQLQueryResult {
 
         private final Map<AbstractRelationalTable<?, ?, ?>, Set<String>> accessedRows; //Table name with respect rows
