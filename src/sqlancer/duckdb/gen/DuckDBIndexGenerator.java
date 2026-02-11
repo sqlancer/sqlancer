e039cbaa71e0dfabc69e816bf5714692f5c5e412
Elshaarawy-1 <am1694553@gmail.com>
2026-02-11 11:36:08 +0200

    Add DuckDBSchema.getIndexes() implementation
    Based on PR #1171
    Addresses #1163

--- src/sqlancer/duckdb/gen/DuckDBIndexGenerator.java
+++ src/sqlancer/duckdb/gen/DuckDBIndexGenerator.java
@@ -23,7 +23,7 @@ public final class DuckDBIndexGenerator {
             sb.append("UNIQUE ");
         }
         sb.append("INDEX ");
-        sb.append(Randomly.fromOptions("i0", "i1", "i2", "i3", "i4")); // cannot query this information
+        sb.append(globalState.getSchema().getFreeIndexName());
         sb.append(" ON ");
         DuckDBTable table = globalState.getSchema().getRandomTable(t -> !t.isView());
         sb.append(table.getName());
@@ -40,7 +40,7 @@ public final class DuckDBIndexGenerator {
             }
         }
         sb.append(")");
-        errors.add("already exists!");
+        // errors.add("already exists!");
         if (globalState.getDbmsSpecificOptions().testRowid) {
             errors.add("cannot create an index on the rowid");
         }
